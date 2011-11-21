1c14a3fa899394310147a0c9164c0492f28c7482
Andrew Nacin <andrewnacin@gmail.com>
2011-11-20 19:58:38 -0500

    Use get_category_feed_link() rather than get_term_feed_link(). Use get_queried_object_id() and get_queried_object() directly. Fix docs/remove i18n.

--- category.php
+++ category.php
@@ -2,8 +2,6 @@
 /**
  * The template for displaying Category Archive pages.
  */
-
-$cat = $wp_query->get_queried_object();
 ?>
 
 <?php get_header(); ?>
@@ -13,18 +11,18 @@ $cat = $wp_query->get_queried_object();
 			<?php if ( have_posts() ) : ?>
 
 			<div class="category-background">
-				<p class="subscribe"><a href="<?php echo get_term_feed_link( $cat->term_id, $cat->taxonomy ); ?>">Follow this category</a></p>
+				<p class="subscribe"><a href="<?php echo esc_url( get_category_feed_link( get_queried_object_id() ) ); ?>">Follow this category</a></p>
 				<h1 class="page-title"><?php single_cat_title(); ?></h1>
 
 				<?php
 					$category_description = category_description();
-					if ( ! empty( $category_description ) )
-						echo apply_filters( 'category_archive_meta', '<div class="category-description">' . $category_description . '</div>' );
+					if ( $category_description )
+						echo '<div class="category-description">' . $category_description . '</div>';
 				?>
 
 				<div class="related-topics clearfix">
 					<h4>Key topics in this category:</h4>
-					<?php echo argo_get_related_topics_for_category( $cat ); ?>
+					<?php echo argo_get_related_topics_for_category( get_queried_object() ); ?>
 				</div> <!-- /.related-topics -->
 			</div> <!-- /.category-background -->
 			
@@ -35,7 +33,7 @@ $cat = $wp_query->get_queried_object();
 				<?php 
 				/* Run the loop for the category page to output the posts.
 				* If you want to overload this in a child theme then include a file
-				* called loop-category.php and that will be used instead.
+				* called content-category.php and that will be used instead.
 				*/
 				get_template_part( 'content', 'category' ); ?>
 
@@ -45,11 +43,11 @@ $cat = $wp_query->get_queried_object();
 
 				<article id="post-0" class="post no-results not-found">
 					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'argo' ); ?></h1>
+						<h1 class="entry-title">Nothing Found</h1>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+						<p>Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.</p>
 						<?php get_search_form(); ?>
 					</div><!-- .entry-content -->
 				</article><!-- #post-0 -->
