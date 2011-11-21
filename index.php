6cbb2e483323efbeaa345c0442a3cf8df8c82cf1
Andrew Nacin <andrewnacin@gmail.com>
2011-11-20 21:34:33 -0500

    Minor cleanups, remove i18n, in index.php.

--- index.php
+++ index.php
@@ -13,22 +13,22 @@ get_header(); ?>
 
 			<div id="content" class="grid_8" role="main">
 
-			<?php if ( have_posts() ) : ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'index' ); ?>
-				<?php endwhile; ?>
-				<?php argo_content_nav( 'nav-below' ); ?>
-			<?php else : ?>
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post();
+					get_template_part( 'content', 'index' );
+				endwhile;
+				argo_content_nav( 'nav-below' );
+			else :
+			?>
 
 				<article id="post-0" class="post no-results not-found">
 					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+						<h1 class="entry-title">Nothing Found</h1>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+						<p>Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.</p>
 						<?php get_search_form(); ?>
 					</div><!-- .entry-content -->
 				</article><!-- #post-0 -->
