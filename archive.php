28c83b8039c8b9f52ca7cd523d32c4c2322417a8
Andrew Nacin <andrewnacin@gmail.com>
2011-11-20 19:54:36 -0500

    Remove i18n.

--- archive.php
+++ archive.php
@@ -70,11 +70,11 @@ get_header(); ?>
 
 				<article id="post-0" class="post no-results not-found">
 					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+						<h1 class="entry-title">Nothing Found</h1>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+						<p>Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.</p>
 						<?php get_search_form(); ?>
 					</div><!-- .entry-content -->
 				</article><!-- #post-0 -->
