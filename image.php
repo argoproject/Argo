143e843b99e5cf6a7d15280791906dab156003e9
Andrew Nacin <andrewnacin@gmail.com>
2011-11-20 20:59:16 -0500

    Don't inject an unescaped post title into an attribute. Note this is copy-pasted from Twenty Eleven, so I've reported it to the WP security team. (It is a vulnerability in the technical sense but is not actually exploitable except in weird setups.)

--- image.php
+++ image.php
@@ -16,17 +16,18 @@ get_header(); ?>
 						<div class="entry-meta">
 							<?php
 								$metadata = wp_get_attachment_metadata();
-								printf( __( '<span class="meta-prep meta-prep-entry-date">Published </span> <span class="entry-date"><abbr class="published" title="%1$s">%2$s</abbr></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%7$s</a>', 'argo' ),
+								printf( '<span class="meta-prep meta-prep-entry-date">Published </span> <span class="entry-date"><abbr class="published" title="%1$s">%2$s</abbr></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%8$s</a>',
 									esc_attr( get_the_time() ),
 									get_the_date(),
 									esc_url( wp_get_attachment_url() ),
 									$metadata['width'],
 									$metadata['height'],
 									esc_url( get_permalink( $post->post_parent ) ),
-									get_the_title( $post->post_parent )
+									esc_attr( get_the_title( $post->post_parent ) ),
+									get_the_title( $post->post_parent ),
 								);
 							?>
-							<?php edit_post_link( __( 'Edit', 'argo' ), '<span class="edit-link">', '</span>' ); ?>
+							<?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
 						</div><!-- .entry-meta -->
 
 					</header><!-- .entry-header -->
@@ -64,7 +65,7 @@ get_header(); ?>
 								echo wp_get_attachment_image( $post->ID, array( $attachment_size, 1024 ) ); // filterable image width with 1024px limit for image height.
 								?>
 
-								<?php if ( ! empty( $post->post_excerpt ) ) : ?>
+								<?php if ( $post->post_excerpt ) : ?>
 								<div class="entry-caption">
 									<?php the_excerpt(); ?>
 								</div>
@@ -80,8 +81,6 @@ get_header(); ?>
 					</div><!-- .entry-content -->
 
 				</article><!-- #post-<?php the_ID(); ?> -->
-
-				
 
 			</div><!--/ #content .grid_12-->
 
