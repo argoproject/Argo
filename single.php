75f7eae57157ccb46993a6acac1c37b4774f0779
Andrew Nacin <andrewnacin@gmail.com>
2011-11-20 22:34:07 -0500

    Single.php cleanup and i18n removal.

--- single.php
+++ single.php
@@ -7,17 +7,16 @@ get_header(); ?>
 
 		<div id="content" class="grid_8" role="main">
 
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'content', 'single' ); ?>
-
-					<?php comments_template( '', true ); ?>
-
-				<?php endwhile; // end of the loop. ?>
+				<?php
+				while ( have_posts() ) : the_post();
+					get_template_part( 'content', 'single' );
+					comments_template( '', true );
+				endwhile; // end of the loop.
+				?>
 				<nav>
 					<ul class="post-nav clearfix">
-						<li class="n-post"><?php next_post_link( '<h5>Newer Post</h5> %link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link' ) . '</span>' ); ?></li>
-						<li class="p-post"><?php previous_post_link( '<h5>Older Post</h5> %link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link' ) . '</span> %title' ); ?></li>
+						<li class="n-post"><?php next_post_link( '<h5>Newer Post</h5> %link', '%title <span class="meta-nav">' . '&rarr;' . '</span>' ); ?></li>
+						<li class="p-post"><?php previous_post_link( '<h5>Older Post</h5> %link', '<span class="meta-nav">' . '&larr;' . '</span> %title' ); ?></li>
 					</ul>
 				</nav><!-- .post-nav -->
 
