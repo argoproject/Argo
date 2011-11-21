44f4fb48e524d2b37845a689b8e6e90a4e018eb3
Andrew Nacin <andrewnacin@gmail.com>
2011-11-20 20:10:36 -0500

    First pass cleanup of content-single.

--- content-single.php
+++ content-single.php
@@ -19,13 +19,13 @@
 		<?php the_content(); ?>
 		<div class="post-meta bottom-meta">
             
-            <?php if ( argo_has_custom_taxonomy(get_the_ID()) ): ?>
+            <?php if ( argo_has_custom_taxonomy( get_the_ID() ) ): ?>
 				<ul class="labels clearfix">
 					<li id="term-view">View more:</li>
             		<?php argo_the_post_labels( get_the_ID() ); ?>
         		</ul>
-        	<?php endif; ?> 
-        	
+        	<?php endif; ?>
+
         <?php if ( argo_has_categories_or_tags() ): ?>
     		<h5>Filed under: <?php echo argo_the_categories_and_tags(); ?></h5>
     	<?php endif; ?>
@@ -35,12 +35,14 @@
 </article><!-- #post-<?php the_ID(); ?> -->
 
 <!-- Related posts -->
-<?php if ( get_option('show_related_content', 1) ): ?>
-<?php $rel_topics = argo_get_post_related_topics( 6 ); if ( $rel_topics ) { ?>
+<?php
+if ( get_option( 'show_related_content', true ) ) :
+	if ( $rel_topics = argo_get_post_related_topics( 6 ) ) :
+?>
 	<div id="related-posts" class="idTabs clearfix">
 		<ul id="related-post-nav">
 			<li><h4>MORE POSTS ABOUT</h4></li>
-			<?php foreach ( $rel_topics as $count => $topic ): ?>
+			<?php foreach ( $rel_topics as $count => $topic ) : ?>
 			<li><a href="#rp<?php echo $count; ?>"><?php echo $topic->name; ?></a></li>
 			<?php endforeach; ?>
 		</ul>
@@ -56,10 +58,10 @@
 						<?php if ( has_post_thumbnail( $top_post->ID ) ) { ?>
 							<img src="<?php echo argo_get_post_thumbnail_src( $top_post, '60x60' ); ?>" alt="related" width="60" height="60" />
 						<?php } ?>
-						<p><?php echo argo_get_excerpt( $top_post ); ?> <a href="<?php echo get_permalink( $top_post->ID ); ?>" title="<?php echo esc_attr($topic->name); ?>" title="<?php echo esc_attr($topic->name); ?>"><b>Read More</b></a></p>
+						<p><?php echo argo_get_excerpt( $top_post ); ?> <a href="<?php echo esc_url( get_permalink( $top_post->ID ) ); ?>" title="<?php echo esc_attr($topic->name); ?>"><b>Read More</b></a></p>
 					</li>
 					<?php foreach ( $rel_posts as $rel_post ): ?>
-					<li><a href="<?php echo get_permalink( $rel_post->ID ); ?>" title="<?php echo esc_attr($topic->name); ?>"><?php echo $rel_post->post_title; ?></a></li>
+					<li><a href="<?php echo esc_url( get_permalink( $rel_post->ID ) ); ?>" title="<?php echo esc_attr($topic->name); ?>"><?php echo $rel_post->post_title; ?></a></li>
 					<?php endforeach; ?>
 				</ul>
 				<p><a href="<?php echo get_term_link( $topic, $topic->taxonomy ); ?>" title="<?php echo esc_attr($topic->name); ?>" target="_blank"><strong>view all <?php echo $topic->name; ?> posts</strong></a></p>
@@ -67,5 +69,6 @@
 		<?php endforeach; ?>
 	</div> <!-- /.items -->
 </div> <!-- /#related-posts -->
-<?php } // if ( $rel_posts ) ?>
-<?php endif; ?>
+<?php
+	endif; // if ( $rel_topics )
+endif; ?>
