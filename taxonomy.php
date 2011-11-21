a4af4e120f4e9a94cb621372052f56b4e2ed1c6b
Andrew Nacin <andrewnacin@gmail.com>
2011-11-20 22:42:30 -0500

    tag.php and taxonomy.php cleanups. Use get_tag_feed_link() in tag.php, use single_term_title() in taxonomy.php. Remove i18n. Improve code formatting. term_description() does not require a taxonomy when working with the currently queried term.

--- taxonomy.php
+++ taxonomy.php
@@ -3,48 +3,41 @@
  * The template for displaying content type archives
  */
 
-$term = $wp_query->get_queried_object();
-?>
-
-<?php  get_header();?>
+get_header(); ?>
 
 		<div id="content" class="grid_8" role="main">
 
 			<?php if ( have_posts() ) : ?>
 
 			<div class="category-background">
-				<p class="subscribe"><a href="<?php echo get_term_feed_link( $cat->term_id, $cat->taxonomy ); ?>">Follow this topic</a></p>
-				<h1 class="page-title"><?php echo $term->name; ?></h1>
+				<p class="subscribe"><a href="<?php echo esc_url( get_term_feed_link( get_queried_object() ) ); ?>">Follow this topic</a></p>
+				<h1 class="page-title"><?php single_term_title(); ?></h1>
 
 				<?php
-					$termDescription = term_description( '', get_query_var( 'taxonomy' ) );
-					if ( ! empty( $termDescription ) )
-						echo apply_filters( 'term_archive_meta', '<div class="taxonomy-description">' . $termDescription . '</div>' );
+					$term_description = term_description();
+					if ( $term_description )
+						echo '<div class="taxonomy-description">' . $term_description . '</div>';
 				?>
 			</div> <!-- /.category-background -->
 			
 			<h3 class="recent-posts">Recent posts</h3>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>				
-				<?php 
-				/* Run the loop for the category page to output the posts.
-				* If you want to overload this in a child theme then include a file
-				* called loop-category.php and that will be used instead.
-				*/
-				get_template_part( 'content', 'category' ); ?>
+			<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
+					get_template_part( 'content', 'taxonomy' ); 
+				endwhile;
+				argo_content_nav( 'nav-below' );
 
-				<?php endwhile; ?>
-				<?php argo_content_nav( 'nav-below' ); ?>
-			<?php else : ?>
+			else : ?>
 
 				<article id="post-0" class="post no-results not-found">
 					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'argo' ); ?></h1>
+						<h1 class="entry-title">Nothing Found</h1>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'argo' ); ?></p>
+						<p>Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.</p>
 						<?php get_search_form(); ?>
 					</div><!-- .entry-content -->
 				</article><!-- #post-0 -->
