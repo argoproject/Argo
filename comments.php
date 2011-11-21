ef073182b260a55d85a1a407adbe8f2fd9a2c651
Andrew Nacin <andrewnacin@gmail.com>
2011-11-20 20:02:30 -0500

    Remove i18n. Leave _n() as it is better than comments_number().

--- comments.php
+++ comments.php
@@ -10,7 +10,7 @@
 ?>
 	<div id="comments">
 	<?php if ( post_password_required() ) : ?>
-		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'argo' ); ?></p>
+		<p class="nopassword">This post is password protected. Enter the password to view any comments.</p>
 	</div><!-- #comments -->
 	<?php
 			/* Stop the rest of comments.php from being processed,
@@ -26,15 +26,15 @@
 	<?php if ( have_comments() ) : ?>
 		<h2 id="comments-title">
 			<?php
-				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'argo' ),
+				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number() ),
 					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
 		</h2>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
 		<nav id="comment-nav-above">
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'argo' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'argo' ) ); ?></div>
+			<div class="nav-previous"><?php previous_comments_link( '&larr; Older Comments' ); ?></div>
+			<div class="nav-next"><?php next_comments_link( 'Newer Comments &rarr;' ); ?></div>
 		</nav>
 		<?php endif; // check for comment navigation ?>
 
@@ -52,8 +52,8 @@
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
 		<nav id="comment-nav-below">
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'argo' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'argo' ) ); ?></div>
+			<div class="nav-previous"><?php previous_comments_link( '&larr; Older Comments' ); ?></div>
+			<div class="nav-next"><?php next_comments_link( 'Newer Comments &rarr;' ); ?></div>
 		</nav>
 		<?php endif; // check for comment navigation ?>
 
@@ -63,7 +63,7 @@
 		 */
 		elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-		<p class="nocomments notice"><?php _e( 'Comments are closed.', 'argo' ); ?></p>
+		<p class="nocomments notice">Comments are closed.</p>
 	<?php endif; ?>
 
 	<?php comment_form(); ?>
