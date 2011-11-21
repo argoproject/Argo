b96caf8809ba66409274a6bc06fa554fe4a8decd
Andrew Nacin <andrewnacin@gmail.com>
2011-11-20 22:20:08 -0500

    Remove i18n, add pluralization for search results, and tidy.

--- search.php
+++ search.php
@@ -10,25 +10,26 @@ get_header(); ?>
 			<?php if ( have_posts() ) : ?>
 
 				<header class="page-header">
-					<h1 class="search-term"><?php _e('Your Search for', 'argo');?> '<?php the_search_query(); ?>' <?php _e('returned', 'argo');?> <strong><?php $NumResults = $wp_query->found_posts; echo $NumResults; ?> <?php _e('results', 'argo');?></strong></h1>
+					<h1 class="search-term">Your Search for <?php the_search_query(); ?> returned <strong><?php printf( _n( '%s result', '%s results', $wp_query->found_posts ), number_format_i18n( $wp_query->found_posts ) ); ?> results</strong></h1>
 				</header>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'search' ); ?>
-				<?php endwhile; ?>
-    			<?php argo_pagination() ?>
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
+					get_template_part( 'content', 'search' );
+				endwhile;
+    			argo_pagination(); ?>
 				<!-- /.search-pagination -->
 
 			<?php else : ?>
 
 				<article id="post-0" class="post no-results not-found">
 					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'argo' ); ?></h1>
+						<h1 class="entry-title">Nothing Found</h1>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
-						<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'argo' ); ?></p>
+						<p>Sorry, but nothing matched your search criteria. Please try again with some different keywords.</p>
 					</div><!-- .entry-content -->
 				</article><!-- #post-0 -->
 
