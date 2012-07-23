e1f16ee746369197d33e4785654d5d15bef3aa3b
Adam Schweigert <aschweigert@gmail.com>
2012-07-23 14:31:35 -0400

    add user options

--- inc/users.php
+++ inc/users.php
@@ -1,51 +1,31 @@
 <?php
+    function largo_contactmethods( $contactmethods ) {
 
-add_action( 'show_user_profile', 'argo_staff_fields' );
-add_action( 'edit_user_profile', 'argo_staff_fields' );
+	     // Remove Yahoo IM
+	    if ( isset( $contactmethods['yim'] ) )
+	    unset( $contactmethods['yim'] );
 
-function argo_staff_fields( $user ) { ?>
+	    // Remove AIM
+	    if ( isset( $contactmethods['aim'] ) )
+	    unset( $contactmethods['aim'] );
 
-<h3>Argo User Settings</h3>
-	<table class="form-table">
-	
-		<tr>
-			<th><label for="argo_twitter">Twitter username</label></th>
-			<td>
-				<input type="text" name="argo_twitter" id="argo_twitter" value="<?php echo esc_attr( get_the_author_meta( 'argo_twitter', $user->ID ) ); ?>" class="regular-text" /><br />
-				<span class="description">Please enter your Twitter username</span>
-			</td>
-		</tr>
-		
-		<tr>
-		    <th>Blog Host?</th>
-		    <td>
-		        <input type="checkbox" name="argo_is_staff" id="argo_is_staff" value="1" <?php checked( 1, get_the_author_meta( 'argo_is_staff', $user->ID ) ); ?> /> <label for="argo_is_staff">Yes</label><br />
-		        <span class="description">Blog hosts appear in the blog host widget.</span>
-		    </td>
-		</tr>
-	</table>
-    <?php
-}
+	    // Remove Jabber
+	    if ( isset( $contactmethods['jabber'] ) )
+	    unset( $contactmethods['jabber'] );
 
-add_action( 'personal_options_update', 'argo_update_staff_fields' );
-add_action( 'edit_user_profile_update', 'argo_update_staff_fields' );
+        // Add a format hint for G+
+	    $contactmethods['googleplus'] = 'Google+<br><em>https://plus.google.com/userID/<em>';
 
-function argo_update_staff_fields( $user_id ) {
+	    // Add Twitter
+	    if ( !isset( $contactmethods['twitter'] ) )
+	    $contactmethods['twitter'] = 'Twitter<br><em>https://twitter.com/username<em>';
 
-	if ( !current_user_can( 'edit_user', $user_id ) )
-		return false;
+	    // Add Facebook
+	    if ( !isset( $contactmethods['facebook'] ) )
+	    $contactmethods['Facebook'] = 'Facebook<br><em>https://www.facebook.com/username<em>';
 
-	update_usermeta( $user_id, 'argo_twitter', $_POST['argo_twitter'] );
-	update_usermeta( $user_id, 'argo_is_staff', $_POST['argo_is_staff']);
-}
+	    return $contactmethods;
+	}
 
-function argo_get_staff() {
-    return get_users(
-            array(
-                'meta_key' => 'argo_is_staff',
-                'meta_value' => 1
-            )
-        );
-}
-
+	add_filter( 'user_contactmethods', 'largo_contactmethods', 10, 1 );
 ?>
