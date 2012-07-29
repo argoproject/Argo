8b3236cf17e9330f32a7c055934e2b958afea153
Adam Schweigert <aschweigert@gmail.com>
2012-07-28 20:23:08 -0400

    make the top header look nicer on mobile devices

--- header.php
+++ header.php
@@ -6,7 +6,11 @@
 <!--[if (gt IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
+
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
 <title><?php
 	/*
 	 * Print the <title> tag based on what is being viewed.
