8b26d4dc4896c25247190cb9bb93a61dd740bf02
Adam Schweigert <aschweigert@gmail.com>
2012-07-30 14:11:57 -0400

    minor edit to 404 page

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@ get_header(); ?>
 
 <div id="content" class="span8" role="main">
 	<h1 class="entry-title">Not Found</h1>
-	<p>Sorry. We can't find the page you were looking for. Please try searching for related posts:</p>
+	<p>Sorry. We can't find the page you were looking for. Maybe searching will help:</p>
 	<?php get_search_form(); ?>
 </div><!-- /.grid_8 #content -->
 <aside id="sidebar" class="span4">
