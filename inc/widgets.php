2002911a63ca584f8266928e5982bb3a94d58bd3
Adam Schweigert <aschweigert@gmail.com>
2012-08-05 23:32:50 -0400

    cleanup and add donation widget

--- inc/widgets.php
+++ inc/widgets.php
@@ -4,6 +4,7 @@ function largo_load_widgets() {
     register_widget( 'largo_follow_widget' );
     register_widget( 'largo_featured_widget' );
     register_widget( 'largo_about_widget' );
+    register_widget( 'largo_donate_widget' );
 }
 add_action( 'widgets_init', 'largo_load_widgets' );
 
@@ -82,7 +83,7 @@ class largo_follow_widget extends WP_Widget {
 	function form( $instance ) {
 
 		/* Set up some default widget settings. */
-		$defaults = array( 'title' => 'Follow Us' );
+		$defaults = array( 'title' => 'Follow ' . get_bloginfo('name') );
 		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
@@ -97,28 +98,19 @@ class largo_follow_widget extends WP_Widget {
  */
 class largo_featured_widget extends WP_Widget {
 
-	/**
-	 * Widget setup.
-	 */
 	function largo_featured_widget() {
-		/* Widget settings. */
 		$widget_ops = array(
 		'classname' => 'largo-featured',
 		'description' => __('Show two recent featured posts with thumbnails and excerpts', 'largo-featured') );
 
-		/* Create the widget. */
 		$this->WP_Widget( 'largo-featured-widget', __('Largo Featured Posts', 'largo-featured'), $widget_ops);
 	}
 
-	/**
-	 * How to display the widget on the screen.
-	 */
 	function widget( $args, $instance ) {
 		extract( $args );
 		$title = apply_filters('widget_title', $instance['title'] );
-		echo $before_widget;
 
-		/* Display the widget title if one was input */
+		echo $before_widget;
 		if ( $title )
 			echo $before_title . $title . $after_title;?>
 
@@ -137,32 +129,20 @@ class largo_featured_widget extends WP_Widget {
          <?php endif; // end more featured posts ?>
 
 		<?php
-		/* After widget (defined by themes). */
 		echo $after_widget;
 	}
 
-	/**
-	 * Update the widget settings.
-	 */
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-
-		/* Strip tags for title */
 		$instance['title'] = strip_tags( $new_instance['title'] );
-
 		return $instance;
 	}
 
-	/**
-	 * Displays the widget settings controls on the widget panel.
-	 */
 	function form( $instance ) {
 
-		/* Set up some default widget settings. */
 		$defaults = array( 'title' => __('In Case You Missed It', 'largo-featured'));
 		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
 
-		<!-- Widget Title: Text Input -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'largo-featured'); ?></label>
 			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:90%;" />
@@ -178,34 +158,22 @@ class largo_featured_widget extends WP_Widget {
  */
 class largo_about_widget extends WP_Widget {
 
-	/**
-	 * Widget setup.
-	 */
 	function largo_about_widget() {
-		/* Widget settings. */
 		$widget_ops = array(
 		'classname' => 'largo-about',
 		'description' => __('Show the site description from your theme options page') );
 
-		/* Create the widget. */
 		$this->WP_Widget( 'largo-about-widget', __('Largo About Site', 'largo-about'), $widget_ops);
 	}
 
-	/**
-	 * How to display the widget on the screen.
-	 */
 	function widget( $args, $instance ) {
 		extract( $args );
 
-		/* Our variables from the widget settings. */
 		$title = apply_filters('widget_title', $instance['title'] );
 
-		/* Before widget (defined by themes). */
 		echo $before_widget;
-
-		/* Display the widget title if one was input (before and after defined by themes). */
 		if ( $title )
-			echo $before_title . $title . $after_title;?>
+			echo $before_title . $title . $after_title; ?>
 
 			<?php if ( of_get_option( 'site_blurb' ) ) : ?>
                 <p><?php echo of_get_option( 'site_blurb' ); ?></p>
@@ -214,40 +182,94 @@ class largo_about_widget extends WP_Widget {
         	<?php endif; // end about site ?>
 
 		<?php
-		/* After widget (defined by themes). */
 		echo $after_widget;
 	}
 
-	/**
-	 * Update the widget settings.
-	 */
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-
-		/* Strip tags for title and name to remove HTML (important for text inputs). */
 		$instance['title'] = strip_tags( $new_instance['title'] );
-
 		return $instance;
 	}
 
-	/**
-	 * Displays the widget settings controls on the widget panel.
-	 * Make use of the get_field_id() and get_field_name() function
-	 * when creating your form elements. This handles the confusing stuff.
-	 */
 	function form( $instance ) {
-
-		/* Set up some default widget settings. */
-		$defaults = array( 'title' => __('About this site', 'largo-about'));
+		$defaults = array( 'title' => __('About ' . get_bloginfo('name'), 'largo-about'));
 		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
 
-		<!-- Widget Title: Text Input -->
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'largo-about'); ?></label>
 			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:90%;" />
 		</p>
-
-
 	<?php
+	}
+}
+class largo_donate_widget extends WP_Widget {
+
+	function largo_donate_widget() {
+
+		$widget_opts = array(
+		'classname' => 'largo-donate',
+		'description'=>__("Call-to-action for donations"));
+		$this->WP_Widget('largo-donate-widget', __('Largo Donate Widget', 'largo-donate'),$widget_opts);
+
+	}
+	function widget( $args, $instance ) {
+		extract( $args );
+
+		$title = apply_filters('widget_title', $instance['title'] );
+
+		echo $before_widget;
+		if ( $title )
+			echo $before_title . $title . $after_title; ?>
+
+            <p><?php echo $instance['cta_text']; ?></p>
+            <a class="btn btn-primary" href="<?php echo $instance['button_url']; ?>"><?php echo $instance['button_text']; ?></a>
+
+		<?php
+		echo $after_widget;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['cta_text'] = strip_tags( $new_instance['cta_text'] );
+		$instance['button_text'] = strip_tags( $new_instance['button_text'] );
+		$instance['button_url'] = strip_tags( $new_instance['button_url'] );
+
+		return $instance;
+	}
+	function form( $instance ) {
+		$donate_link = '';
+		if ( of_get_option( 'donate_link' ) )
+			$donate_link = esc_url( of_get_option( 'donate_link' ) );
+		$donate_btn_text = 'Donate Now';
+		if ( of_get_option( 'donate_button_text' ) )
+			$donate_btn_text = esc_attr( of_get_option( 'donate_button_text' ) );
+		$defaults = array(
+			'title' => 'Support ' . get_bloginfo('name'),
+			'cta_text' => 'We depend on your support. A generous gift in any amount helps us continue to bring you this service.',
+			'button_text' => $donate_btn_text,
+			'button_url' => $donate_link
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'largo-donate'); ?></label>
+			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:90%;" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'cta_text' ); ?>">Call-to-Action Text:</label>
+			<input id="<?php echo $this->get_field_id( 'cta_text' ); ?>" name="<?php echo $this->get_field_name( 'cta_text' ); ?>" value="<?php echo $instance['cta_text']; ?>" style="width:90%;" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'button_text' ); ?>">Button Text:</label>
+			<input id="<?php echo $this->get_field_id( 'button_text' ); ?>" name="<?php echo $this->get_field_name( 'button_text' ); ?>" value="<?php echo $instance['button_text']; ?>" style="width:90%;" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'button_url' ); ?>">Button URL (for custom campaigns):</label>
+			<input id="<?php echo $this->get_field_id( 'button_url' ); ?>" name="<?php echo $this->get_field_name( 'button_url' ); ?>" value="<?php echo $instance['button_url']; ?>" style="width:90%;" />
+		</p>
+
+		<?php
 	}
 }
