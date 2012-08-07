a6527ec4d3b2f35c9582a86255fd7d72fb0ec809
Adam Schweigert <aschweigert@gmail.com>
2012-08-06 23:19:07 -0400

    more cleanup

--- inc/widgets.php
+++ inc/widgets.php
@@ -1,22 +1,30 @@
 <?php
 /*
  remove the unsupported default WP widgets
- others (for reference):
- unregister_widget( 'WP_Widget_RSS' );
- unregister_widget( 'WP_Widget_Text' );
- unregister_widget( 'WP_Widget_Archives' );
- unregister_widget( 'WP_Widget_Categories' );
- unregister_widget( 'WP_Widget_Recent_Posts' );
+ complete list (for reference):
+ 	unregister_widget('WP_Widget_Pages');
+ 	unregister_widget('WP_Widget_Calendar');
+ 	unregister_widget('WP_Widget_Archives');
+ 	unregister_widget('WP_Widget_Links');
+ 	unregister_widget('WP_Widget_Meta');
+ 	unregister_widget('WP_Widget_Search');
+ 	unregister_widget('WP_Widget_Text');
+ 	unregister_widget('WP_Widget_Categories');
+ 	unregister_widget('WP_Widget_Recent_Posts');
+ 	unregister_widget('WP_Widget_Recent_Comments');
+ 	unregister_widget('WP_Widget_RSS');
+ 	unregister_widget('WP_Widget_Tag_Cloud');
+ 	unregister_widget('WP_Nav_Menu_Widget');
 */
-
 function largo_unregister_widgets() {
 	unregister_widget( 'WP_Widget_Pages' );
 	unregister_widget( 'WP_Widget_Calendar' );
 	unregister_widget( 'WP_Widget_Links' );
-	unregister_widget( 'WP_Widget_Search' );
 	unregister_widget( 'WP_Widget_Tag_Cloud' );
 	unregister_widget( 'WP_Widget_Meta' );
 	unregister_widget( 'WP_Widget_Recent_Comments' );
+	unregister_widget( 'WP_Nav_Menu_Widget' );
+	unregister_widget( 'WP_Widget_RSS' );
 }
 add_action( 'widgets_init', 'largo_unregister_widgets' );
 
