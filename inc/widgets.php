30f999587087b096bd69c66c8a0c65287d0de337
Adam Schweigert <aschweigert@gmail.com>
2012-08-06 23:07:38 -0400

    remove unused default widgets

--- inc/widgets.php
+++ inc/widgets.php
@@ -1,5 +1,28 @@
 <?php
+/*
+ remove the unsupported default WP widgets
+ others (for reference):
+ unregister_widget( 'WP_Widget_RSS' );
+ unregister_widget( 'WP_Widget_Text' );
+ unregister_widget( 'WP_Widget_Archives' );
+ unregister_widget( 'WP_Widget_Categories' );
+ unregister_widget( 'WP_Widget_Recent_Posts' );
+*/
 
+function largo_unregister_widgets() {
+	unregister_widget( 'WP_Widget_Pages' );
+	unregister_widget( 'WP_Widget_Calendar' );
+	unregister_widget( 'WP_Widget_Links' );
+	unregister_widget( 'WP_Widget_Search' );
+	unregister_widget( 'WP_Widget_Tag_Cloud' );
+	unregister_widget( 'WP_Widget_Meta' );
+	unregister_widget( 'WP_Widget_Recent_Comments' );
+}
+add_action( 'widgets_init', 'largo_unregister_widgets' );
+
+/*
+ * ...and then register our new widgets
+ */
 function largo_load_widgets() {
     register_widget( 'largo_follow_widget' );
     register_widget( 'largo_footer_featured_widget' );
