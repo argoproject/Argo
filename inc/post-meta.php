41e3b1bf1074f605342bcc66bb6f5d6bd83cd0d8
Adam Schweigert <aschweigert@gmail.com>
2012-08-06 23:20:53 -0400

    a little reorganization (+ turn the admin bar back on)

--- inc/post-meta.php
+++ inc/post-meta.php
@@ -106,4 +106,18 @@ function largo_show_author_box() {
 		return false;
 	}
 }
+
+//detemine whether or not an author has a valid gravatar image, see: http://codex.wordpress.org/Using_Gravatars
+function has_gravatar($email) {
+	// Craft a potential url and test its headers
+	$hash = md5(strtolower(trim($email)));
+	$uri = 'http://www.gravatar.com/avatar/' . $hash . '?d=404';
+	$headers = @get_headers($uri);
+	if (!preg_match("|200|", $headers[0])) {
+		$has_valid_avatar = FALSE;
+	} else {
+		$has_valid_avatar = TRUE;
+	}
+	return $has_valid_avatar;
+}
 ?>
