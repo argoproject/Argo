e08666845138eba3ff7c65ec38cdd96ed302816f
Adam Schweigert <aschweigert@gmail.com>
2012-08-09 00:12:12 -0400

    update tweet button code at the bottom of posts

--- content-single.php
+++ content-single.php
@@ -31,7 +31,7 @@
 
 	<div class="post-social bottom">
  			<div class="left">
- 				<span class="twitter"><a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" data-count="none">Tweet</a></span>
+ 				<span class="twitter"><a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php echo the_permalink(); ?>" data-text="<?php echo the_title(); ?>" <?php if ( of_get_option( 'twitter_link' ) ) : ?>data-via="<?php echo twitter_url_to_username( of_get_option( 'twitter_link' ) ); ?>"<?php endif; ?> data-count="none">Tweet</a></span>
  				<span class="facebook"><fb:like href="<?php echo the_permalink(); ?>" send="false" layout="button_count" show_faces="false" action="recommend" font=""></fb:like></span>
  			</div>
  			<div class="right">
