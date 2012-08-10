a2302b7fcd5790d4deef7e461ce4895a7b7f18fa
Adam Schweigert <aschweigert@gmail.com>
2012-08-09 22:09:53 -0400

    update default sidebar widgets

--- sidebar-topic.php
+++ sidebar-topic.php
@@ -8,6 +8,21 @@
 		if ( ! dynamic_sidebar( 'topic-sidebar' ) ) :
 			the_widget( 'largo_follow_widget', array( 'title' => 'Follow Us' ) );
 			the_widget( 'largo_about_widget', array( 'title' => 'About This Site' ) );
+			if ( of_get_option( 'donate_link' ) )
+				the_widget( 'largo_donate_widget', array(
+					'title' => 'Support ' . get_bloginfo('name'),
+					'cta_text' => 'We depend on your support. A generous gift in any amount helps us continue to bring you this service.',
+					'button_text' => 'Donate Now',
+					'button_url' => esc_url( of_get_option( 'donate_link' ) ),
+					'widget_class' => 'default'
+					)
+				);
+			the_widget( 'largo_sidebar_featured_widget', array(
+					'title' => 'We Recommend',
+					'num_posts' => 5,
+					'widget_class' => 'default'
+				)
+			);
 		endif;
 	?>
 </div><!-- #main .widget-area -->
