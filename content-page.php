fe1cc6d9b0e80a0ca76af89bd023474cfe6a7afe
Adam Schweigert <aschweigert@gmail.com>
2012-08-16 20:53:46 -0400

    add clearfix for consistency

--- content-page.php
+++ content-page.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
