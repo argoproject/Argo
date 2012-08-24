24814a43f685c079a253b7c33f5c15b0349c2042
Adam Schweigert <aschweigert@gmail.com>
2012-08-24 12:15:00 -0400

    update to a newer version that is actively maintained

--- js/jquery.textPlaceholder.js
+++ js/jquery.textPlaceholder.js
@@ -1,44 +1,11 @@
-/**
- * @see http://github.com/NV/placeholder.js
- */
-jQuery.fn.textPlaceholder = function () {
+/*
+* Placeholder plugin for jQuery
+* ---
+* Copyright 2010, Daniel Stocks (http://webcloud.se)
+* Released under the MIT, BSD, and GPL Licenses.
+*/
 
-	return this.each(function(){
-
-		var that = this;
-
-		if (that.placeholder && 'placeholder' in document.createElement(that.tagName)) return;
-
-		var placeholder = that.getAttribute('placeholder');
-		var input = jQuery(that);
-
-		if (that.value === '' || that.value == placeholder) {
-			input.addClass('text-placeholder');
-			that.value = placeholder;
-		}
-
-		input.focus(function(){
-			if (input.hasClass('text-placeholder')) {
-				this.value = '';
-				input.removeClass('text-placeholder')
-			}
-		});
-
-		input.blur(function(){
-			if (this.value === '') {
-				input.addClass('text-placeholder');
-				this.value = placeholder;
-			} else {
-				input.removeClass('text-placeholder');
-			}
-		});
-
-		that.form && jQuery(that.form).submit(function(){
-			if (input.hasClass('text-placeholder')) {
-				that.value = '';
-			}
-		});
-
-	});
-
-};
+(function(b){function d(a){this.input=a;a.attr("type")=="password"&&this.handlePassword();b(a[0].form).submit(function(){if(a.hasClass("placeholder")&&a[0].value==a.attr("placeholder"))a[0].value=""})}d.prototype={show:function(a){if(this.input[0].value===""||a&&this.valueIsPlaceholder()){if(this.isPassword)try{this.input[0].setAttribute("type","text")}catch(b){this.input.before(this.fakePassword.show()).hide()}this.input.addClass("placeholder");this.input[0].value=this.input.attr("placeholder")}},
+hide:function(){if(this.valueIsPlaceholder()&&this.input.hasClass("placeholder")&&(this.input.removeClass("placeholder"),this.input[0].value="",this.isPassword)){try{this.input[0].setAttribute("type","password")}catch(a){}this.input.show();this.input[0].focus()}},valueIsPlaceholder:function(){return this.input[0].value==this.input.attr("placeholder")},handlePassword:function(){var a=this.input;a.attr("realType","password");this.isPassword=!0;if(b.browser.msie&&a[0].outerHTML){var c=b(a[0].outerHTML.replace(/type=(['"])?password\1/gi,
+"type=$1text$1"));this.fakePassword=c.val(a.attr("placeholder")).addClass("placeholder").focus(function(){a.trigger("focus");b(this).hide()});b(a[0].form).submit(function(){c.remove();a.show()})}}};var e=!!("placeholder"in document.createElement("input"));b.fn.placeholder=function(){return e?this:this.each(function(){var a=b(this),c=new d(a);c.show(!0);a.focus(function(){c.hide()});a.blur(function(){c.show(!1)});b.browser.msie&&(b(window).load(function(){a.val()&&a.removeClass("placeholder");c.show(!0)}),
+a.focus(function(){if(this.value==""){var a=this.createTextRange();a.collapse(!0);a.moveStart("character",0);a.select()}}))})}})(jQuery);
