b03665f603f96550dd64f28393ebb39b4d9ffec1
Adam Schweigert <aschweigert@gmail.com>
2012-10-01 11:30:05 -0400

    fix a bug where a duplicate child term was sometimes being inserted in the post prominence taxonomy

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -9,13 +9,16 @@ function largo_custom_taxonomies() {
             'query_var' => true,
             'rewrite' => true,
         ) );
-        wp_insert_term( 'Homepage Featured', 'prominence', array( 'description' => 'If you are using the Newspaper or Carousel optional homepage layout, add this label to posts to display them in the featured area on the homepage' ) );
-        wp_insert_term( 'Sidebar Featured Widget', 'prominence', array( 'description' => 'If you are using the Sidebar Featured Posts widget, add this label to posts to determine which to display in the widget' ) );
-        wp_insert_term( 'Footer Featured Widget', 'prominence', array( 'description' => 'If you are using the Footer Featured Posts widget, add this label to posts to determine which to display in the widget' ) );
+        wp_insert_term( 'Homepage Featured', 'prominence', array( 'description' => 'If you are using the Newspaper or Carousel optional homepage layout, add this label to posts to display them in the featured area on the homepage', 'slug' => 'homepage-featured' ) );
+        wp_insert_term( 'Sidebar Featured Widget', 'prominence', array( 'description' => 'If you are using the Sidebar Featured Posts widget, add this label to posts to determine which to display in the widget', 'slug' => 'sidebar-featured' ) );
+        wp_insert_term( 'Footer Featured Widget', 'prominence', array( 'description' => 'If you are using the Footer Featured Posts widget, add this label to posts to determine which to display in the widget', 'slug' => 'footer-featured' ) );
 
-        $parent_term = term_exists( 'Homepage Featured', 'prominence' );
-        $parent_term_id = $parent_term['term_id'];
-        wp_insert_term( 'Top Story', 'prominence', array( 'parent'=> $parent_term_id, 'description' => 'If you are using the Newspaper or Carousel optional homepage layout, add this label to a post to make it the top story on the homepage' ) );
+        $term = term_exists('Top Story', 'prominence');
+        if ( $term == 0 || $term == null ) {
+		    $parent_term = term_exists( 'Homepage Featured', 'prominence' );
+		    $parent_term_id = $parent_term['term_id'];
+		    wp_insert_term( 'Top Story', 'prominence', array( 'parent'=> $parent_term_id, 'description' => 'If you are using the Newspaper or Carousel optional homepage layout, add this label to a post to make it the top story on the homepage', 'slug' => 'homepage-featured-top-story' ) );
+		}
     }
 
     // SERIES
