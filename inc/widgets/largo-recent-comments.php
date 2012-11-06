1f7e8fd40ad7e3756aca495e56d567c492a18fee
Adam Schweigert <aschweigert@gmail.com>
2012-11-06 18:30:00 -0500

    add a widget to display recent comments

---
+++ inc/widgets/largo-recent-comments.php
@@ -0,0 +1,163 @@
+<?php
+/*
+ * Largo recent-comments Widget
+ */
+
+class largo_recent_comments_widget extends WP_Widget {
+
+	/**
+	 * Widget setup.
+	 */
+	function largo_recent_comments_widget() {
+		/* Widget settings. */
+		$widget_ops = array(
+			'classname' 	=> 'largo-recent-comments',
+			'description' 	=> 'Show recent comments',
+		);
+
+		/* Create the widget. */
+		$this->WP_Widget( 'largo-recent-comments-widget', 'Largo Recent Comments', $widget_ops );
+		$this->alt_option_name = 'largo_recent_comments';
+
+		add_action( 'comment_post', array(&$this, 'flush_widget_cache') );
+		add_action( 'transition_comment_status', array(&$this, 'flush_widget_cache') );
+	}
+
+	function flush_widget_cache() {
+		wp_cache_delete('largo_recent_comments', 'widget');
+	}
+
+	/**
+	 * How to display the widget on the screen.
+	 */
+	function widget( $args, $instance ) {
+		global $comments, $comment;
+
+		$cache = wp_cache_get('widget_recent_comments', 'widget');
+
+		if ( ! is_array( $cache ) )
+			$cache = array();
+
+		if ( ! isset( $args['widget_id'] ) )
+			$args['widget_id'] = $this->id;
+
+		if ( isset( $cache[ $args['widget_id'] ] ) ) {
+			echo $cache[ $args['widget_id'] ];
+			return;
+		}
+
+		extract( $args );
+		$output = '';
+		$title = apply_filters('widget_title', $instance['title'] );
+
+		if ( empty( $instance['number'] ) || ! $number = absint( $instance['number'] ) )
+ 			$number = 5;
+
+		$comments = get_comments( apply_filters( 'widget_comments_args', array( 'number' => $number, 'status' => 'approve', 'post_status' => 'publish' ) ) );
+
+		$widget_class = !empty($instance['widget_class']) ? $instance['widget_class'] : '';
+		if ($instance['hidden_desktop'] === 1)
+			$widget_class .= ' hidden-desktop';
+		if ($instance['hidden_tablet'] === 1)
+			$widget_class .= ' hidden-tablet';
+		if ($instance['hidden_phone'] === 1)
+			$widget_class .= ' hidden-phone';
+		/* Add the widget class to $before widget, used as a style hook */
+		if( strpos($before_widget, 'class') === false ) {
+			$before_widget = str_replace('>', 'class="'. $widget_class . '"', $before_widget);
+		}
+		else {
+			$before_widget = str_replace('class="', 'class="'. $widget_class . ' ', $before_widget);
+		}
+
+		$output .= $before_widget;
+		if ( $title )
+			$output .= $before_title . $title . $after_title;
+
+		$output .= '<ul id="recentcomments">';
+		if ( $comments ) {
+			foreach ( (array) $comments as $comment) {
+				$output .=  '<li class="recentcomments">';
+				$output .= '<p class="comment-excerpt">' . get_comment_excerpt() . '</p>';
+				$output .= '<p class="comment-meta">&mdash;&nbsp;' . get_comment_author_link() . ' on <a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . get_the_title($comment->comment_post_ID) . '</a></p>';
+				$output .= '</li>';
+			}
+ 		}
+		$output .= '</ul>';
+		$output .= $after_widget;
+
+		echo $output;
+		$cache[$args['widget_id']] = $output;
+		wp_cache_set('widget_recent_comments', $cache, 'widget');
+	}
+
+	/**
+	 * Update the widget settings.
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['number'] = absint( $new_instance['number'] );
+		$this->flush_widget_cache();
+
+		$alloptions = wp_cache_get( 'alloptions', 'options' );
+		if ( isset($alloptions['largo_recent_comments']) )
+			delete_option('largo_recent_comments');
+
+		$instance['widget_class'] = $new_instance['widget_class'];
+		$instance['hidden_desktop'] = $new_instance['hidden_desktop'] ? 1 : 0;
+		$instance['hidden_tablet'] = $new_instance['hidden_tablet'] ? 1 : 0;
+		$instance['hidden_phone'] = $new_instance['hidden_phone'] ? 1 : 0;
+		return $instance;
+	}
+
+	/**
+	 * Displays the widget settings controls on the widget panel.
+	 */
+	function form( $instance ) {
+
+		/* Set up some default widget settings. */
+		$defaults = array(
+			'title' 			=> '',
+			'number'			=> 5,
+			'widget_class' 		=> 'default',
+			'hidden_desktop' 	=> '',
+			'hidden_tablet' 	=> '',
+			'hidden_phone'		=> ''
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		$title = strip_tags($instance['title']);
+		$number = isset($instance['number']) ? absint($instance['number']) : 5;
+		$desktop = $instance['hidden_desktop'] ? 'checked="checked"' : '';
+		$tablet = $instance['hidden_tablet'] ? 'checked="checked"' : '';
+		$phone = $instance['hidden_phone'] ? 'checked="checked"' : '';
+
+		?>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $instance['title'] ); ?>" style="width:90%;" />
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of comments to show:'); ?></label>
+		<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" />
+		</p>
+
+		<label for="<?php echo $this->get_field_id( 'widget_class' ); ?>"><?php _e('Widget Background', 'largo-recent-comments'); ?></label>
+		<select id="<?php echo $this->get_field_id('widget_class'); ?>" name="<?php echo $this->get_field_name('widget_class'); ?>" class="widefat" style="width:90%;">
+		    <option <?php selected( $instance['widget_class'], 'default'); ?> value="default">Default</option>
+		    <option <?php selected( $instance['widget_class'], 'rev'); ?> value="rev">Reverse</option>
+		    <option <?php selected( $instance['widget_class'], 'no-bg'); ?> value="no-bg">No Background</option>
+		</select>
+
+		<p style="margin:15px 0 10px 5px">
+			<input class="checkbox" type="checkbox" <?php echo $desktop; ?> id="<?php echo $this->get_field_id('hidden_desktop'); ?>" name="<?php echo $this->get_field_name('hidden_desktop'); ?>" /> <label for="<?php echo $this->get_field_id('hidden_desktop'); ?>"><?php _e('Hidden on Desktops?'); ?></label>
+			<br />
+			<input class="checkbox" type="checkbox" <?php echo $tablet; ?> id="<?php echo $this->get_field_id('hidden_tablet'); ?>" name="<?php echo $this->get_field_name('hidden_tablet'); ?>" /> <label for="<?php echo $this->get_field_id('hidden_tablet'); ?>"><?php _e('Hidden on Tablets?'); ?></label>
+			<br />
+			<input class="checkbox" type="checkbox" <?php echo $phone; ?> id="<?php echo $this->get_field_id('hidden_phone'); ?>" name="<?php echo $this->get_field_name('hidden_phone'); ?>" /> <label for="<?php echo $this->get_field_id('hidden_phone'); ?>"><?php _e('Hidden on Phones?'); ?></label>
+		</p>
+	<?php
+	}
+}
