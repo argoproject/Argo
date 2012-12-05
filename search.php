d150580ce8191042fa34c2ec670eeef14d9a263b
Adam Schweigert <aschweigert@gmail.com>
2012-12-05 09:32:16 -0500

    avoid duplicating the search form

--- search.php
+++ search.php
@@ -9,14 +9,7 @@ get_header(); ?>
 
 			<?php if ( have_posts() ) { ?>
 
-				<header class="clearfix">
-					<form class="form-search" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-						<div>
-							<input type="text" placeholder="Search" class="searchbox search-query" value="<?php the_search_query(); ?>" name="s" />
-							<input type="submit" value="GO" name="search submit" class="search-submit btn">
-						</div>
-					</form>
-				</header>
+				<?php get_search_form(); ?>
 
 				<h3 class="recent-posts clearfix">Your search for <span class="search-term"><?php the_search_query(); ?></span> returned <?php printf( _n( '%s result', '%s results', $wp_query->found_posts ), number_format_i18n( $wp_query->found_posts ) ); ?></h3>
 
