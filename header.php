8c6a0e9e71b833a5b0f534455db2ebc8ede6a239
Adam Schweigert <aschweigert@gmail.com>
2012-12-11 12:36:58 -0500

    a line break here seems to add weird extra space, removing it

--- header.php
+++ header.php
@@ -77,8 +77,7 @@
 				<div id="header-search">
 					<form class="form-search" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 						<div class="input-append">
-							<input type="text" placeholder="<?php _e('Search', 'largo'); ?>" class="input-medium appendedInputButton search-query" value="" name="s" />
-							<button type="submit" class="search-submit btn"><?php _e('GO', 'largo'); ?></button>
+							<input type="text" placeholder="<?php _e('Search', 'largo'); ?>" class="input-medium appendedInputButton search-query" value="" name="s" /><button type="submit" class="search-submit btn"><?php _e('GO', 'largo'); ?></button>
 						</div>
 					</form>
 				</div>
