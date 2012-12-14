722ecec002ed82c3fce1b5e68e84f282a15323cf
Adam Schweigert <aschweigert@gmail.com>
2012-12-14 14:23:11 -0500

    comment walker was removed by mistake, readding it

--- inc/post-tags.php
+++ inc/post-tags.php
@@ -335,3 +335,67 @@ if ( ! function_exists( 'largo_content_nav' ) ) {
 		<?php endif;
 	}
 }
+
+/**
+ * Template for comments and pingbacks.
+ *
+ * Used as a callback by wp_list_comments() for displaying the comments.
+ */
+if ( ! function_exists( 'largo_comment' ) ) {
+	function largo_comment( $comment, $args, $depth ) {
+		$GLOBALS['comment'] = $comment;
+		switch ( $comment->comment_type ) :
+			case 'pingback' :
+			case 'trackback' :
+		?>
+		<li class="post pingback">
+			<p>Pingback: <?php comment_author_link(); ?><?php edit_comment_link( 'Edit', '<span class="edit-link">', '</span>' ); ?></p>
+		<?php
+				break;
+			default :
+		?>
+		<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+			<article id="comment-<?php comment_ID(); ?>" class="comment">
+				<footer class="comment-meta">
+					<div class="comment-author vcard">
+						<?php
+							$avatar_size = 68;
+							if ( '0' != $comment->comment_parent )
+								$avatar_size = 39;
+
+							echo get_avatar( $comment, $avatar_size );
+
+							/* translators: 1: comment author, 2: date and time */
+							printf( '%1$s on %2$s <span class="says">said:</span>',
+								sprintf( '<span class="fn">%s</span>', get_comment_author_link() ),
+								sprintf( '<a href="%1$s"><time pubdate datetime="%2$s">%3$s</time></a>',
+									esc_url( get_comment_link( $comment->comment_ID ) ),
+									get_comment_time( 'c' ),
+									/* translators: 1: date, 2: time */
+									sprintf( '%1$s at %2$s', get_comment_date(), get_comment_time() )
+								)
+							);
+						?>
+
+						<?php edit_comment_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
+					</div><!-- .comment-author .vcard -->
+
+					<?php if ( $comment->comment_approved == '0' ) : ?>
+						<em class="comment-awaiting-moderation">Your comment is awaiting moderation.</em>
+						<br />
+					<?php endif; ?>
+
+				</footer>
+
+				<div class="comment-content"><?php comment_text(); ?></div>
+
+				<div class="reply">
+					<?php comment_reply_link( array_merge( $args, array( 'reply_text' => 'Reply <span>&darr;</span>', 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+				</div><!-- .reply -->
+			</article><!-- #comment-## -->
+
+		<?php
+				break;
+		endswitch;
+	}
+} // ends check for largo_comment()
