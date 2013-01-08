3c5b1ae36dbb3abe97b88a7b537bbb2608d2fa22
Adam Schweigert <aschweigert@gmail.com>
2013-01-08 17:43:42 -0500

    somehow missed the twitter widget in the previous commit
    whoops.

--- inc/widgets/largo-twitter.php
+++ inc/widgets/largo-twitter.php
@@ -15,21 +15,6 @@ class largo_twitter_widget extends WP_Widget {
 	function widget( $args, $instance ) {
 		extract( $args );
 
-		$widget_class = !empty($instance['widget_class']) ? $instance['widget_class'] : '';
-		if ($instance['hidden_desktop'] === 1)
-			$widget_class .= ' hidden-desktop';
-		if ($instance['hidden_tablet'] === 1)
-			$widget_class .= ' hidden-tablet';
-		if ($instance['hidden_phone'] === 1)
-			$widget_class .= ' hidden-phone';
-		/* Add the widget class to $before_widget, used as a style hook */
-		if( strpos($before_widget, 'class') === false ) {
-			$before_widget = str_replace('>', 'class="'. $widget_class . '"', $before_widget);
-		}
-		else {
-			$before_widget = str_replace('class="', 'class="'. $widget_class . ' ', $before_widget);
-		}
-
 		echo $before_widget;
 
 			 $widget_embed = '<a class="twitter-timeline" height="500" href="https://twitter.com/';
@@ -68,10 +53,6 @@ class largo_twitter_widget extends WP_Widget {
 		$instance['widget_ID'] = strip_tags( $new_instance['widget_ID'] );
 		$instance['widget_type'] = $new_instance['widget_type'];
 		$instance['widget_theme'] = $new_instance['widget_theme'];
-		$instance['widget_class'] = $new_instance['widget_class'];
-		$instance['hidden_desktop'] = $new_instance['hidden_desktop'] ? 1 : 0;
-		$instance['hidden_tablet'] = $new_instance['hidden_tablet'] ? 1 : 0;
-		$instance['hidden_phone'] = $new_instance['hidden_phone'] ? 1 : 0;
 		return $instance;
 	}
 
@@ -82,16 +63,9 @@ class largo_twitter_widget extends WP_Widget {
 			'twitter_list_slug' => 'inn-staff-and-associates',
 			'twitter_search' 	=> __('your search', 'largo'),
 			'widget_type' 		=> 'timeline',
-			'widget_theme' 		=> 'light',
-			'widget_class' 		=> 'default',
-			'hidden_desktop' 	=> '',
-			'hidden_tablet' 	=> '',
-			'hidden_phone'		=> ''
+			'widget_theme' 		=> 'light'
 		);
 		$instance = wp_parse_args( (array) $instance, $defaults );
-		$desktop = $instance['hidden_desktop'] ? 'checked="checked"' : '';
-		$tablet = $instance['hidden_tablet'] ? 'checked="checked"' : '';
-		$phone = $instance['hidden_phone'] ? 'checked="checked"' : '';
 		?>
 
 		<p>
@@ -130,21 +104,6 @@ class largo_twitter_widget extends WP_Widget {
 		<p>
 			<label for="<?php echo $this->get_field_id( 'twitter_search' ); ?>"><?php _e('Twitter Search Query (for search widget):', 'largo'); ?></label>
 			<input id="<?php echo $this->get_field_id( 'twitter_search' ); ?>" name="<?php echo $this->get_field_name( 'twitter_search' ); ?>" value="<?php echo $instance['twitter_search']; ?>" style="width:90%;" />
-		</p>
-
-		<label for="<?php echo $this->get_field_id( 'widget_class' ); ?>"><?php _e('Widget Background', 'largo'); ?></label>
-		<select id="<?php echo $this->get_field_id('widget_class'); ?>" name="<?php echo $this->get_field_name('widget_class'); ?>" class="widefat" style="width:90%;">
-		    <option <?php selected( $instance['widget_class'], 'default'); ?> value="default"><?php _e('Default', 'largo'); ?></option>
-		    <option <?php selected( $instance['widget_class'], 'rev'); ?> value="rev"><?php _e('Reverse', 'largo'); ?></option>
-		    <option <?php selected( $instance['widget_class'], 'no-bg'); ?> value="no-bg"><?php _e('No Background', 'largo'); ?></option>
-		</select>
-
-		<p style="margin:15px 0 10px 5px">
-			<input class="checkbox" type="checkbox" <?php echo $desktop; ?> id="<?php echo $this->get_field_id('hidden_desktop'); ?>" name="<?php echo $this->get_field_name('hidden_desktop'); ?>" /> <label for="<?php echo $this->get_field_id('hidden_desktop'); ?>"><?php _e('Hidden on Desktops?', 'largo'); ?></label>
-			<br />
-			<input class="checkbox" type="checkbox" <?php echo $tablet; ?> id="<?php echo $this->get_field_id('hidden_tablet'); ?>" name="<?php echo $this->get_field_name('hidden_tablet'); ?>" /> <label for="<?php echo $this->get_field_id('hidden_tablet'); ?>"><?php _e('Hidden on Tablets?', 'largo'); ?></label>
-			<br />
-			<input class="checkbox" type="checkbox" <?php echo $phone; ?> id="<?php echo $this->get_field_id('hidden_phone'); ?>" name="<?php echo $this->get_field_name('hidden_phone'); ?>" /> <label for="<?php echo $this->get_field_id('hidden_phone'); ?>"><?php _e('Hidden on Phones?', 'largo'); ?></label>
 		</p>
 
 	<?php
