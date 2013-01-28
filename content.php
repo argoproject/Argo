d32772e606342cb16a26ae9ec2ee16385357939f
adam schweigert <aschweigert@gmail.com>
2013-01-28 14:59:51 -0500

    consolidate content.php and content-home.php (because they're more or less the same)

--- content.php
+++ content.php
@@ -2,25 +2,31 @@
 /**
  * The default template for displaying content
  */
+global $tags;
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
 
 	<header>
+ 		<?php if ( is_home() && largo_has_categories_or_tags() && $tags === 'top' ) { ?>
+	 		<h5 class="top-tag"><?php largo_categories_and_tags( 1 ); ?></h5>
+	 	<?php } ?>
+
  		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="Permalink to <?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
- 		<div class="post-meta">
- 			<h5 class="byline"><?php largo_byline(); ?><?php edit_post_link( __('Edit This Post', 'largo'), ' | <span class="edit-link">', '</span>'); ?></h5>
- 		</div>
+
+ 		<h5 class="byline"><?php largo_byline(); ?><?php edit_post_link( __('Edit This Post', 'largo'), ' | <span class="edit-link">', '</span>'); ?></h5>
+
 	</header><!-- / entry header -->
 
 	<div class="entry-content">
 		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+
 		<?php largo_excerpt( $post, 5, true, __('Continue&nbsp;Reading&nbsp;&rarr;', 'largo') ); ?>
-        <?php if ( largo_has_categories_or_tags() ): ?>
-            <div class="post-meta bottom-meta">
-    			<h5 class="tag-list"><strong><?php _e('Filed under:', 'largo'); ?></strong> <?php largo_categories_and_tags(); ?></h5>
-            </div><!-- /.post-meta -->
-        <?php endif; ?>
+
+        <?php if ( !is_home() || ( largo_has_categories_or_tags() && $tags === 'btm' ) ) { ?>
+    		<h5 class="tag-list"><strong><?php _e('Filed under:', 'largo'); ?></strong> <?php largo_categories_and_tags( 8 ); ?></h5>
+    	<?php } ?>
+
 	</div><!-- .entry-content -->
 
 </article><!-- #post-<?php the_ID(); ?> -->
