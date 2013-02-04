63815e846ba7518662a9aed00eedfda8eb69b753
adam schweigert <aschweigert@gmail.com>
2013-02-04 18:18:19 -0500

    add a polite reminder to leave the credit line in place

--- footer.php
+++ footer.php
@@ -19,6 +19,7 @@
 			<p><?php largo_copyright_message(); ?></p>
 			<?php wp_nav_menu( array( 'theme_location' => 'footer-bottom', 'container' => false, 'depth' => 1  ) ); ?>
 			<div class="footer-bottom clearfix">
+				<!-- If you enjoy this theme and use it on a production site we would appreciate it if you would leave the credit in place. Thanks :) -->
 				<p class="footer-credit"><?php _e('This site built with <a href="http://largoproject.org">Project Largo</a> from the <a href="http://investigativenewsnetwork.org">Investigative News Network</a> and proudly powered by <a href="http://wordpress.org" rel="nofollow">WordPress</a>.', 'largo'); ?></p>
 				<p class="back-to-top"><a href="#top"><?php _e('Back to top &uarr;', 'largo'); ?></a></p>
 			</div>
