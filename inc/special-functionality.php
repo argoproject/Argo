0b876a8cf6deab06835091a8d0514fde325c7711
adam schweigert <aschweigert@gmail.com>
2013-03-15 14:31:17 -0400

    add a function to generate full text rss feed
    even when a site only shows excerpts in their main feed

--- inc/special-functionality.php
+++ inc/special-functionality.php
@@ -158,3 +158,16 @@ function largo_admin_menu() {
      remove_menu_page('link-manager.php');
 }
 add_action( 'admin_menu', 'largo_admin_menu' );
+
+/**
+ * Create a full text RSS feed even if site is using excerpts in the main feed
+ * URL for full text feed is: http://mysite.org/?feed=fulltext
+ *
+ * @since 1.0
+ */
+
+function largo_full_text_feed() {
+    add_filter('pre_option_rss_use_excerpt', '__return_zero');
+    load_template( ABSPATH . WPINC . '/feed-rss2.php' );
+}
+add_feed('fulltext', 'largo_full_text_feed');
