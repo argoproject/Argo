9679671118f5e2bec228f9d8bc28f163dc94e137
adam schweigert <aschweigert@gmail.com>
2013-03-26 18:21:21 -0400

    update 404.php to use the new sidebar formatting

--- 404.php
+++ 404.php
@@ -6,9 +6,7 @@ get_header(); ?>
 
 <div id="content" class="span8" role="main">
 	<?php get_template_part( 'content', 'not-found' ); ?>
-</div><!-- /.grid_8 #content -->
-<aside id="sidebar" class="span4">
+</div><!--#content -->
+
 <?php get_sidebar(); ?>
-</aside>
-<!-- /.grid_4 -->
 <?php get_footer(); ?>
