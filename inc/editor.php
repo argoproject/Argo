740e5e859c4a7785e2c67cb8ef2aef312d46f6e3
Ben Byrne <ben@byrnecreative.com>
2013-04-01 18:13:48 +0000

    Building out embed code for media

--- inc/editor.php
+++ inc/editor.php
@@ -6,20 +6,20 @@
  * @since 1.0
  */
 function add_largo_mce_plugin( $plugin_array ) {
-    $plugin_array['modulize'] = get_template_directory_uri() . '/js/tinymce/plugins/largo/editor_plugin.js';
-    return $plugin_array;
+	$plugin_array['modulize'] = get_template_directory_uri() . '/js/tinymce/plugins/largo/editor_plugin.js';
+	return $plugin_array;
 }
 
 function register_largo_mce_buttons( $buttons ) {
-    array_push( $buttons, '|', 'modulize' );
-    return $buttons;
+	array_push( $buttons, '|', 'modulize' );
+	return $buttons;
 }
 
 function largo_add_mce_buttons() {
-    if ( get_user_option( 'rich_editing' ) == 'true' ) {
-        add_filter( 'mce_external_plugins', 'add_largo_mce_plugin', 4 );
-        add_filter( 'mce_buttons', 'register_largo_mce_buttons', 4 );
-    }
+	if ( get_user_option( 'rich_editing' ) == 'true' ) {
+		add_filter( 'mce_external_plugins', 'add_largo_mce_plugin', 4 );
+		add_filter( 'mce_buttons', 'register_largo_mce_buttons', 4 );
+	}
 }
 add_action( 'init', 'largo_add_mce_buttons' );
 
@@ -30,13 +30,13 @@ add_action( 'init', 'largo_add_mce_buttons' );
  * @since 1.0
  */
 function module_shortcode( $atts, $content, $code ) {
-    extract( shortcode_atts( array(
-        'align' => 'left',
-        'width' => 'half',
-        'type' => 'aside',
-    ), $atts ) );
+	extract( shortcode_atts( array(
+		'align' => 'left',
+		'width' => 'half',
+		'type' => 'aside',
+	), $atts ) );
 
-    return sprintf( '<aside class="module %s %s %s">%s</aside>', $type, $align, $width, $content );
+	return sprintf( '<aside class="module %s %s %s">%s</aside>', $type, $align, $width, $content );
 }
 add_shortcode( 'module', 'module_shortcode' );
 
@@ -46,11 +46,11 @@ add_shortcode( 'module', 'module_shortcode' );
  * @since 1.0
  */
 function move_author_to_publish_metabox() {
-    global $post_ID;
-    $post = get_post( $post_ID );
-    echo '<div id="author" class="misc-pub-section" style="padding: 8px 10px;">Author: ';
-    post_author_meta_box( $post );
-    echo '</div>';
+	global $post_ID;
+	$post = get_post( $post_ID );
+	echo '<div id="author" class="misc-pub-section" style="padding: 8px 10px;">Author: ';
+	post_author_meta_box( $post );
+	echo '</div>';
 }
 add_action( 'post_submitbox_misc_actions', 'move_author_to_publish_metabox' );
 
--- js/tinymce/plugins/largo/tinymce.js
+++ js/tinymce/plugins/largo/tinymce.js
@@ -1,19 +1,76 @@
 function init() {
-    tinyMCEPopup.resizeToInnerSize();
+	tinyMCEPopup.resizeToInnerSize();
+	the_content = tinyMCE.getInstanceById('content').selection.getContent();
+	console.log( the_content );
+	console.log('here');
 }
 
 function insertModuleCode(){
-    var inst = tinyMCE.getInstanceById('content');
-    var html = inst.selection.getContent();
+	var inst = tinyMCE.getInstanceById('content'),
+		html = inst.selection.getContent(),
+		mod_width = $("input[name='mod_width']:checked").val(),
+		mod_align = $("input[name='mod_align']:checked").val(),
+		mod_type  = $("input[name='mod_type']:checked").val(),
+		embed_style = "";
 
-    var mod_width = $("input[name='mod_width']:checked").val();
-    var mod_align = $("input[name='mod_align']:checked").val();
-    var mod_type  = $("input[name='mod_type']:checked").val();
+	if (mod_type == 'embed' && mod_width == 'extract') {
+		var dims = getEmbedDimensions( html );
+		if (dims) embed_style = 'style="' + dims + '"';
+	}
 
-    var start_tag = '[module align="' + mod_align + '" width="' + mod_width + '" type="' + mod_type + '"]';
+	var start_tag = '<aside class="module align-' + mod_align + ' width-' + mod_width + ' type-' + mod_type + '" ' + embed_style + '>';
 
-    window.tinyMCE.execInstanceCommand('content', 'mceInsertContent', false, start_tag+html+'[/module]');
-    tinyMCEPopup.editor.execCommand('mceRepaint');
-    tinyMCEPopup.close();
-    return;
+	window.tinyMCE.execInstanceCommand('content', 'mceInsertContent', false, start_tag+html+'</aside>');
+	tinyMCEPopup.editor.execCommand('mceRepaint');
+	tinyMCEPopup.close();
+	return;
 }
+
+//try to extract a width and height value from the markup
+function getEmbedDimensions(markup){
+
+	var style = "";
+	//look for a width
+	var widtharray = /width=('|")([0-9]*)('|")/im.exec(markup);
+	if (widtharray.length == 4) {
+		style = style + "overflow:hidden;width:" + widtharray[2] + "px;";
+	} else {
+		return false;
+	}
+
+	//then a height, too, just because
+	var heightarray = /height=('|")([0-9]*)('|")/im.exec(markup);
+	if (heightarray.length == 4) {
+		style = style + "height:" + heightarray[2] + "px;";
+	}
+	return style;
+}
+
+/**
+ * Some UX improvements for embedded media
+ */
+jQuery(document).ready(function($) {
+
+	//watch the mod_type and trigger the embed change
+	$("input[name='mod_type']").click(function() {
+		widthToggle();
+	});
+
+	function widthToggle() {
+		$ex = $("#extract");
+		if ( $("input[name='mod_type']:checked").val() == 'embed')
+			$ex.removeAttr('disabled');
+		else {
+			//uncheck it
+			if ( $ex.prop('checked') ) {
+				$ex.removeAttr('checked');
+				console.log( $("#half") );
+				$("#half").prop('checked','checked');
+			}
+			//disable it
+			$ex.attr('disabled','disabled');
+		}
+	}
+
+	widthToggle();
+});
