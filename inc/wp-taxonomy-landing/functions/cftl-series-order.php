3c9827f613a7f46bf14ef5017523918186a31a68
adam schweigert <aschweigert@gmail.com>
2013-05-14 12:26:34 -0400

    merging the latest version of the series landing page stuff
    closes #92

---
+++ inc/wp-taxonomy-landing/functions/cftl-series-order.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @package taxonomy-landing
+ *
+ * This file is part of Taxonomy Landing for WordPress
+ * https://github.com/crowdfavorite/wp-taxonomy-landing
+ *
+ * Copyright (c) 2009-2012 Crowd Favorite, Ltd. All rights reserved.
+ * http://crowdfavorite.com
+ *
+ * **********************************************************************
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * **********************************************************************
+ */
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+
+/**
+ * Reorders posts according to custom order
+ * Uses postmeta series_[termID]_order values
+ */
+function largo_series_custom_order ( $sql ) {
+	global $wp_query, $opt, $wpdb;
+
+	//only do this if we're a series page
+	if ( is_array($opt) && $wp_query->query_vars['taxonomy'] == 'series' ) :
+
+		//get the term object to set the proper meta stuff and whatnot
+		$term = get_term_by( 'slug', $wp_query->query_vars['term'], 'series' );
+
+		//custom sort order
+		if ( $opt['post_order'] == 'custom' ) {
+
+			$meta_key = 'series_' . $term->term_id . '_order';
+
+			//retool the query
+			$sql['join'] = "
+				INNER JOIN $wpdb->term_relationships tr ON ($wpdb->posts.ID = tr.object_id)
+				LEFT JOIN $wpdb->postmeta AS meta ON ($wpdb->posts.ID = meta.post_id AND meta.meta_key = '{$meta_key}')";
+			$sql['where'] = "
+				 AND ( tr.term_taxonomy_id IN (".$term->term_id.") )
+				 AND $wpdb->posts.post_type = 'post'
+				 AND ($wpdb->posts.post_status = 'publish' OR $wpdb->posts.post_status = 'private') ";
+			$sql['orderby'] = "ISNULL(meta.meta_value+0) ASC, meta.meta_value+0 ASC, $wpdb->posts.post_date DESC";
+
+		//featured stories first
+		}  elseif ( strpos( $opt['post_order'], 'featured,' ) === 0 ) {
+
+			list( $top, $sort ) = explode( " ", $opt['post_order'] );
+			$top_term = get_term_by( 'slug', 'series-featured', 'prominence' );
+
+			//retool the query
+			$sql['join'] = "
+				INNER JOIN $wpdb->term_relationships ON ($wpdb->posts.ID = $wpdb->term_relationships.object_id)
+				LEFT JOIN $wpdb->term_relationships t2 ON ($wpdb->posts.ID = t2.object_id)
+				AND (t2.term_taxonomy_id = " . $top_term->term_id . ")";
+			$sql['where'] = "
+				AND ( $wpdb->term_relationships.term_taxonomy_id IN (".$term->term_id.") )
+				AND $wpdb->posts.post_type = 'post'
+				AND ($wpdb->posts.post_status = 'publish' OR $wpdb->posts.post_status = 'private') ";
+			$sql['orderby'] = "ISNULL(t2.term_taxonomy_id) ASC, $wpdb->posts.post_date $sort";
+
+		}
+	endif;
+	return $sql;
+
+}
+add_filter( 'posts_clauses', 'largo_series_custom_order');
