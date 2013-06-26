f3cb0bce4b211922e861a79b11df46d38349fefc
adam schweigert <aschweigert@gmail.com>
2013-06-26 14:28:29 -0400

    set a default value if theme options aren't set

--- archive.php
+++ archive.php
@@ -77,7 +77,7 @@ get_header();
 					 * Show a label for the list of recent posts
 					 * again, tailored to the type of page we're looking at
 					 */
-					$posts_term = of_get_option( 'posts_term_plural' );
+					$posts_term = of_get_option( 'posts_term_plural', 'Stories' );
 
 					if ( is_author() ) {
 						printf(__('Recent %1$s<a class="rss-link" href="%2$s"><i class="icon-rss"></i></a>', 'largo'), $posts_term, get_author_feed_link( get_the_author_meta('ID') ) );
