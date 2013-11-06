79cfea25774e17e27f2cbb1d8ceff202d4a45de4
adam schweigert <aschweigert@gmail.com>
2013-11-06 10:34:46 -0500

    adding a custom rss feed for mailchimp
    includes post thumbnails in a format mailchimp can recognize

--- inc/custom-feeds.php
+++ inc/custom-feeds.php
@@ -12,3 +12,17 @@ function largo_full_text_feed() {
     load_template( ABSPATH . WPINC . '/feed-rss2.php' );
 }
 add_feed('fulltext', 'largo_full_text_feed');
+
+/*
+ * Register a custom RSS feed for MailChimp (to include thumbnail images)
+ * Feed address to use for MailChimp import will be http://myurl.com/?feed=mailchimp
+ * And then you'll use the *|RSSITEM:IMAGE|* merge tag in your MailChimp template
+ *
+ * @package Largo
+ * @since 1.0
+ */
+function mailchimp_rss() {
+	add_filter('pre_option_rss_use_excerpt', '__return_zero');
+	load_template( get_stylesheet_directory() . '/feed-mailchimp.php' );
+}
+add_feed('mailchimp', 'mailchimp_rss');
