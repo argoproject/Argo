eed8167c63ef0a35b97769341d84b36f1ad965ba
seamusleahy <seamus@seamusleahy.com>
2013-11-13 16:11:02 -0600

    Adding touch events for the drop-down menus

--- js/largoCore.js
+++ js/largoCore.js
@@ -64,4 +64,51 @@ jQuery(document).ready(function($) {
 	  if (typeof _gaq == 'object') _gaq.push(['_trackEvent', 'Click', 'Image Widget', this.getAttribute('title')]);
   });
 
+  // Touch enable the drop-down menus
+  (function() {
+	  if ( Modernizr.touch ) {
+
+	  	// iOS Safari works with touchstart, the rest work with click
+			mobileEvent = /Mobile\/.+Safari/.test(navigator.userAgent) ? 'touchstart' : 'click';
+
+			// Open the drop down
+			var openMenu = false;
+
+			// Handle the tap for the drop down
+		  $('ul.nav').on( mobileEvent+'.largo', 'li.dropdown', function( event ) {
+		  	var li = $(event.currentTarget);
+		  	console.log(event.target, event.currentTarget);
+
+		  	if ( !li.is('.open') ) {
+		  		// The link when the menu is closed
+		  		closeOpenMenu();
+		  		li.addClass('open');
+		  		openMenu = li;
+
+		  		event.preventDefault();
+		  		event.stopPropagation();
+
+		  	} else if ( $(event.target).is('b.caret') ) {
+		  		// The caret when the menu is open
+		  		li.removeClass('open');
+		  		openMenu = false;
+		  		
+		  		event.preventDefault();
+		  		event.stopPropagation();
+		  	}
+		  });
+
+		  // Call this to call the open menu
+		  var closeOpenMenu = function() {
+		  	if ( openMenu ) {
+		  		openMenu.removeClass('open');
+		  		openMenu = false;
+		  	}
+		  }
+
+		  // Close the open menu when the user taps elsewhere
+		  $('body').on( mobileEvent, closeOpenMenu);
+		}
+	})();
+
 });
