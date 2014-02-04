b47d7f2674a81290134a67d463a6d9ff2f76641f
adam schweigert <aschweigert@gmail.com>
2014-02-03 23:52:40 -0500

    show excerpt box on posts by default

--- inc/post-meta.php
+++ inc/post-meta.php
@@ -32,6 +32,19 @@ function remove_default_post_screen_metaboxes() {
 add_action('admin_menu','remove_default_post_screen_metaboxes');
 
 /**
+ * Show all of the other metaboxes by default (particularly to show the excerpt)
+ *
+ * @since 1.0
+ */
+add_filter( 'default_hidden_meta_boxes', 'largo_change_default_hidden_metaboxes', 10, 2 );
+function largo_change_default_hidden_metaboxes( $hidden, $screen ) {
+    if ( 'post' == $screen->base ) {
+        $hidden = array();
+    }
+    return $hidden;
+}
+
+/**
  * Adds custom meta boxes to the post edit screen for
  *  - custom byline and link
  *  - featured video link
