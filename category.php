2774be111afbc135be2046608d8af92836da8859
Ben Byrne <ben@cornershopcreative.com>
2014-04-07 13:07:03 +0000

    Fixing category archive when thumb is missing

--- category.php
+++ category.php
@@ -47,7 +47,7 @@ global $tags;
 				?>
 				<div class="primary-featured-post">
 					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix row-fluid'); ?>>
-
+					<?php if ( has_post_thumbnail() ) { ?>
 						<div class="span4">
 							<?php if ( is_home() && largo_has_categories_or_tags() && $tags === 'top' ) { ?>
 						 		<h5 class="top-tag"><?php largo_categories_and_tags( 1 ); ?></h5>
@@ -57,6 +57,9 @@ global $tags;
 						</div>
 
 						<div class="span8">
+					<?php } else { ?>
+						<div class="span12">
+					<?php } ?>
 							<header>
 						 		<h2 class="entry-title">
 						 			<a href="<?php the_permalink(); ?>" title="Permalink to <?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
