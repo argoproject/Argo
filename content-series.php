18333bd5ebef0eef67ab85b28c5bc1abdbb74e5d
adam schweigert <aschweigert@gmail.com>
2014-05-30 16:52:28 -0400

    whoops

--- content-series.php
+++ content-series.php
@@ -3,7 +3,6 @@
  * The template for displaying content on a series landing page
  */
 global $opt;	//get display options for the loop
-//print_r ($opt);
 $tags = of_get_option ('tag_display');
 
 ?>
@@ -12,7 +11,7 @@ $tags = of_get_option ('tag_display');
 
 	<header>
 
- 		<?php if ( isset($opt['show']['tags']) && $opt['show']['tags'] === 1 && largo_has_categories_or_tags() && $tags === 'top' ) { ?>
+ 		<?php if ( isset($opt['show']['tags']) && $opt['show']['tags'] == 1 && largo_has_categories_or_tags() && $tags === 'top' ) { ?>
     		<h5 class="top-tag"><?php largo_top_term(); ?></h5>
     	<?php } ?>
 
@@ -20,21 +19,21 @@ $tags = of_get_option ('tag_display');
  			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute( array( 'before' => __( 'Permalink to', 'largo' ) . ' ' ) )?>" rel="bookmark"><?php the_title(); ?></a>
  		</h2>
 
- 		<?php if ( isset($opt['show']['byline']) && $opt['show']['byline'] === 1 ) : ?>
+ 		<?php if ( isset($opt['show']['byline']) && $opt['show']['byline'] == 1 ) : ?>
  		<h5 class="byline"><?php largo_byline(); ?></h5>
  		<?php endif; ?>
 	</header><!-- / entry header -->
 
 	<div class="entry-content">
- 		<?php if ( isset($opt['show']['image']) && $opt['show']['image'] === 1 ) : ?>
+ 		<?php if ( isset($opt['show']['image']) && $opt['show']['image'] == 1 ) : ?>
 		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
 		<?php endif; ?>
 
- 		<?php if ( isset($opt['show']['excerpt']) && $opt['show']['excerpt'] === 1 ) :
+ 		<?php if ( isset($opt['show']['excerpt']) && $opt['show']['excerpt'] == 1 ) :
 			largo_excerpt( $post, 5, true, __('Continue&nbsp;Reading&nbsp;&rarr;', 'largo'), true, false );
 		endif; ?>
 
-		<?php if ( isset($opt['show']['tags']) && $opt['show']['tags'] === 1 && largo_has_categories_or_tags() && $tags === 'btm' ) { ?>
+		<?php if ( isset($opt['show']['tags']) && $opt['show']['tags'] == 1 && largo_has_categories_or_tags() && $tags === 'btm' ) { ?>
     		<h5 class="tag-list"><strong><?php _e('Filed under:', 'largo'); ?></strong> <?php largo_categories_and_tags( 8 ); ?></h5>
     	<?php } ?>
 
