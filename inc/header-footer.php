10033df6adb0f084e1edaf946dbf270b17ac4f07
adam schweigert <aschweigert@gmail.com>
2014-08-19 13:37:21 -0400

    finish busting up post-page-styles.less

--- inc/header-footer.php
+++ inc/header-footer.php
@@ -154,8 +154,6 @@ remove_action( 'wp_head', 'rsd_link' );
 remove_action( 'wp_head', 'wlwmanifest_link' );
 remove_action( 'wp_head', 'wp_generator' );
 
-
-
 /**
  * Add a class to the body on single article pages when in 'classic' mode
  *
