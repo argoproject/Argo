9b9c4e82511eab118d011b8a8c2e492d617bc805
Ryan Nagle <rmnagle@gmail.com>
2014-08-25 19:05:01 -0500

    wip moving homepage-related taxonomy stuff (post prominence) to the homepage layout rig

--- homepages/layouts/HomepageSingleWithFeatured.php
+++ homepages/layouts/HomepageSingleWithFeatured.php
@@ -10,6 +10,46 @@ class HomepageSingleWithFeatured extends HomepageSingle {
 		'Home Bottom Left', 'Home Bottom Center', 'Home Bottom Right'
 	);
 
+	function __construct($options=array()) {
+		$this->prominenceTerms = array(
+			//array(
+				//'name' 			=> __('Homepage Featured', 'largo'),
+				//'description' 	=> __('If you are using the Newspaper or Carousel optional homepage layout, add this label to posts to display them in the featured area on the homepage.', 'largo'),
+				//'slug' 			=> 'homepage-featured'
+			//),
+			array(
+				'name' 			=> __('Sidebar Featured Widget', 'largo'),
+				'description' 	=> __('If you are using the Sidebar Featured Posts widget, add this label to posts to determine which to display in the widget.', 'largo'),
+				'slug' 			=> 'sidebar-featured'
+			),
+			array(
+				'name' 			=> __('Footer Featured Widget', 'largo'),
+				'description' 	=> __('If you are using the Footer Featured Posts widget, add this label to posts to determine which to display in the widget.', 'largo'),
+				'slug' 			=> 'footer-featured'
+			),
+			array(
+				'name' 			=> __('Featured in Series', 'largo'),
+				'description' 	=> __('Select this option to allow this post to float to the top of any/all series landing pages sorting by Featured first.', 'largo'),
+				'slug' 			=> 'series-featured'
+			),
+			array(
+				'name' 			=> __('Featured in Category', 'largo'),
+				'description' 	=> __('This will allow you to designate a story to appear more prominently on category archive pages.', 'largo'),
+				'slug' 			=> 'category-featured'
+			)
+		);
+
+		$defaults = array(
+			'template' => get_template_directory() . '/homepages/templates/full-width-image.php',
+			'assets' => array(
+				array('homepage-single', get_template_directory_uri() . '/homepages/assets/css/single.css', array()),
+				array('homepage-single', get_template_directory_uri() . '/homepages/assets/js/single.js', array('jquery'))
+			)
+		);
+		$options = array_merge($defaults, $options);
+		$this->load($options);
+	}
+
 	public function moreStories() {
 		return homepage_feature_stories_list();
 	}
