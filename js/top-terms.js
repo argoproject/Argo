09d2f1493ae74f2bd941e6c8ff8b188ece13bfef
Ryan Nagle <rmnagle@gmail.com>
2014-09-02 17:06:19 -0500

    some reformatting of top-terms.js

--- js/top-terms.js
+++ js/top-terms.js
@@ -1,28 +1,22 @@
 /**
  * Handles adding/removing options from top_terms admin dropdown
  */
+(function($) {
+  $(document).ready(function() {
+    var $dropdown = $('#top_term'),
+    $checklists = $('.categorychecklist').not("[id^='prominence'], [id^='post-type']");
 
-(function( $ ) {
-
-	$(document).ready(function() {
-
-		var $dropdown = $('#top_term'),
-			$checklists = $('.categorychecklist').not( "[id^='prominence'], [id^='post-type']" );
-
-		$("input[type='checkbox']", $checklists).on( 'click', function() {
-			var value = $(this).val();
-			if (this.checked) {
-				//make sure it exists
-				if ( !$("option[value='"+value+"']", $dropdown).length ) {
-					$dropdown.append('<option value="'+value+'">'+ $(this).parent('label').text().trim() +'</option>');
-				}
-			} else {
-				//make sure it's gone
-				if ( $("option[value='"+value+"']", $dropdown).length ) {
-					$("option[value='"+value+"']", $dropdown).remove();
-				}
-			}
-		});
-	});
-
-})( jQuery );
+    $("input[type='checkbox']", $checklists).on('click', function() {
+      var value = $(this).val();
+      if (this.checked) {
+        //make sure it exists
+        if (!$("option[value='" + value + "']", $dropdown).length)
+          $dropdown.append('<option value="' + value + '">' + $(this).parent('label').text().trim() + '</option>');
+      } else {
+        //make sure it's gone
+        if ($("option[value='" + value + "']", $dropdown).length)
+          $("option[value='" + value + "']", $dropdown).remove();
+      }
+    });
+  });
+})(jQuery);
