3b725cc80f4e112f2391b39d15379bd0adeed567
adam schweigert <aschweigert@gmail.com>
2014-09-17 12:15:47 -0400

    hookin'

--- footer.php
+++ footer.php
@@ -9,33 +9,33 @@
 
 </div><!-- #page -->
 
-<?php if ( is_active_sidebar( 'before-footer' ) ) : ?>
-<div class="before-footer-wrapper">
-	<div id="before-footer">
-		<?php dynamic_sidebar( 'before-footer' ); ?>
-	</div>
-</div>
-<?php endif; ?>
+<?php
+	if ( is_active_sidebar( 'before-footer' ) ) {
+		get_template_part( 'partials/footer', 'before-footer-widget-area' );
+	}
+?>
+
+<?php do_action( 'largo_before_footer' ); ?>
 
 <div class="footer-bg clearfix">
 	<footer id="site-footer">
-		<div id="supplementary" class="row-fluid">
-			<?php get_template_part( 'partials/footer-part', 'widget-area' ); ?>
-		</div>
-		<div id="boilerplate" class="row-fluid clearfix">
-			<p><?php largo_copyright_message(); ?></p>
-			<?php largo_cached_nav_menu( array( 'theme_location' => 'footer-bottom', 'container' => false, 'depth' => 1  ) ); ?>
-			<div class="footer-bottom clearfix">
-				<!-- If you enjoy this theme and use it on a production site we would appreciate it if you would leave the credit in place. Thanks :) -->
-				<p class="footer-credit"><?php echo sprintf( __('This site built with <a href="%s">Project Largo</a> from the <a href="%s">Investigative News Network</a> and proudly powered by <a href="%s" rel="nofollow">WordPress</a>.', 'largo'), 'http://largoproject.org', 'http://investigativenewsnetwork.org', 'http://wordpress.org' ); ?></p>
 
-				<p class="back-to-top"><a href="#top"><?php _e('Back to top', 'largo'); ?> &uarr;</a></p>
-			</div>
-		</div><!-- /#boilerplate -->
+		<?php do_action( 'largo_before_footer_widgets' ); ?>
+
+		<?php get_template_part( 'partials/footer', 'widget-area' ); ?>
+
+		<?php do_action( 'largo_before_footer_boilerplate' ); ?>
+
+		<?php get_template_part( 'partials/footer', 'boilerplate' ); ?>
+
+		<?php do_action( 'largo_before_footer_close' ); ?>
+
 	</footer>
 </div>
 
-<?php get_template_part( 'partials/largo-sticky-footer' ); ?>
+<?php do_action( 'largo_after_footer' ); ?>
+
+<?php get_template_part( 'partials/footer', 'sticky' ); ?>
 
 <?php wp_footer(); ?>
 
--- partials/nav-secondary.php
+++ partials/nav-secondary.php
@@ -1,9 +1,6 @@
-<?php
+<nav id="secondary-nav" class="clearfix">
+	<div id="topics-bar" class="span12 hidden-phone">
+		<?php largo_cached_nav_menu( array( 'theme_location' => 'dont-miss', 'container' => false, 'depth' => 1 ) ); ?>
+	</div>
+</nav>
 
-if ( of_get_option( 'show_dont_miss_menu') ) { ?>
-	<nav id="secondary-nav" class="clearfix">
-		<div id="topics-bar" class="span12 hidden-phone">
-			<?php largo_cached_nav_menu( array( 'theme_location' => 'dont-miss', 'container' => false, 'depth' => 1 ) ); ?>
-		</div>
-	</nav>
-<?php }
