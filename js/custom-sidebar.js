fc59666c55d047aac4fd39df508c305834f4214b
Ryan Nagle <rmnagle@gmail.com>
2014-10-07 10:09:28 -0500

    when the post layout changes, switch the sidebar to the default value for that layout

--- js/custom-sidebar.js
+++ js/custom-sidebar.js
@@ -2,34 +2,38 @@
  * Handles changing default sidebar option based on which post/page layout option is selected
  */
 (function($) {
-  $(document).ready(function() {
-    var post_template = $('#post_template');
-    var custom_sidebar = $('#custom_sidebar');
-    var none_option = $(custom_sidebar.find('[value="none"]'));
-    var default_text = none_option.text();
+    $(document).ready(function() {
+        var post_template = $('#post_template');
+        var custom_sidebar = $('#custom_sidebar');
+        var none_option = $(custom_sidebar.find('[value="none"]'));
+        var default_text = none_option.text();
 
-    post_template.on('change', function() {
-      if (post_template.val() == '') {
-        if (post_template.text().match(/Two Column/g))
-          none_option.text(default_sidebar_labels['single-two-column.php']);
+        post_template.on('change', function() {
+            custom_sidebar.find('option').removeAttr('selected');
 
-        if (post_template.text().match(/One Column/g))
-          none_option.text(default_sidebar_labels['single-one-column.php']);
+            if (post_template.val() == '') {
+                if ($(post_template.find('option').get(0)).text().match(/Two Column/g))
+                    none_option.text(default_sidebar_labels['single-two-column.php']);
 
-        custom_sidebar.removeAttr('disabled');
-      }
+                if ($(post_template.find('option').get(0)).text().match(/One Column/g))
+                    none_option.text(default_sidebar_labels['single-one-column.php']);
 
-      if (default_sidebar_labels[post_template.val()]) {
-        none_option.text(default_sidebar_labels[post_template.val()]);
-        custom_sidebar.removeAttr('disabled');
-      }
+                custom_sidebar.removeAttr('disabled');
+            }
 
-      if (post_template.val() == 'full-page.php') {
-        custom_sidebar.find('option').removeAttr('selected');
-        none_option.attr('selected', 'selected');
-        none_option.text(default_sidebar_labels[post_template.val()]);
-        custom_sidebar.attr('disabled', 'disabled');
-      }
+            if (default_sidebar_labels[post_template.val()]) {
+                none_option.text(default_sidebar_labels[post_template.val()]);
+                custom_sidebar.removeAttr('disabled');
+            }
+
+            if (post_template.val() == 'full-page.php') {
+                custom_sidebar.find('option').removeAttr('selected');
+                none_option.attr('selected', 'selected');
+                none_option.text(default_sidebar_labels[post_template.val()]);
+                custom_sidebar.attr('disabled', 'disabled');
+            }
+
+            none_option.attr('selected', 'selected');
+        });
     });
-  });
 })(jQuery);
