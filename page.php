3095123c2e08d96b24d07d361560cb6f8ed2bc2e
Ryan Nagle <rmnagle@gmail.com>
2014-10-15 14:39:36 -0500

    set up page template to use the same classic/normal switch as single post template

--- page.php
+++ page.php
@@ -1,16 +1,5 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The Template for displaying all pages.
  */
-get_header();
-?>
-
-<div id="content" class="span8" role="main">
-	<?php
-		the_post();
-		get_template_part( 'partials/content', 'page' );
-	?>
-</div><!-- #content -->
-
-<?php get_sidebar(of_get_option('single_template')); ?>
-<?php get_footer(); ?>
+include_once __DIR__ . '/single.php';
