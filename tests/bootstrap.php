884313ae44e9db65945e31b15bc83b1db8e98595
Ryan Nagle <rmnagle@gmail.com>
2014-11-16 08:48:59 -0600

    more debugging of issues running tests on travis ci

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,11 @@
 $wp_tests_dir = getenv('WP_TESTS_DIR');
 require_once $wp_tests_dir . '/includes/functions.php';
 
+$basename = basename(dirname(__DIR__));
+
 $GLOBALS['wp_tests_options'] = array(
-	'stylesheet' => 'largo-dev',
-	'template' => 'largo-dev'
+	'stylesheet' => $basename,
+	'template' => $basename
 );
 
 tests_add_filter('set_current_user', function($arg) {
@@ -15,7 +17,7 @@ tests_add_filter('set_current_user', function($arg) {
 }, 1, 10);
 
 tests_add_filter('filesystem_method', function($arg) {
-	return 'Direct';
+	return 'direct';
 }, 1, 10);
 
 require dirname(__FILE__) . '/inc/mock-options-framework.php';
