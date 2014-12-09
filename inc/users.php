c03b5012e8bd2125592cd97669f97d8fce989b18
Ryan Nagle <rmnagle@gmail.com>
2014-12-09 13:02:58 -0600

    include the staff roster widget from the nerds site in largo core widgets

--- inc/users.php
+++ inc/users.php
@@ -353,3 +353,51 @@ function largo_render_staff_list_shortcode($atts=array()) {
 	largo_render_user_list(largo_get_user_list($args), $show_users_with_empty_desc);
 }
 add_shortcode('roster', 'largo_render_staff_list_shortcode');
+
+function more_profile_info($user) {
+	$hide = get_user_meta( $user->ID, "hide", true );
+	$emeritus = get_user_meta( $user->ID, "emeritus", true );
+	$honorary = get_user_meta( $user->ID, "honorary", true );
+	?>
+	<h3>More profile information</h3>
+	<table class="form-table">
+		<tr>
+			<th><label for="job_title">Job title</label></th>
+			<td>
+				<input type="text" name="job_title" id="job_title" value="<?php echo esc_attr( get_the_author_meta( 'job_title', $user->ID ) ); ?>" class="regular-text" /><br />
+				<span class="description">Please enter your job title.</span>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="staff_widget">Staff status</label></th>
+			<td>
+				<input type="checkbox" name="hide" id="hide"
+					<?php if (esc_attr($hide) == "on") { ?>checked<?php }?> />
+				<label for="hide"><?php _e("Hide in roster"); ?></label><br />
+
+				<input type="checkbox" name="emeritus" id="emeritus"
+					<?php if (esc_attr($emeritus) == "on") { ?>checked<?php } ?> />
+				<label for="emeritus"><?php _e("Emeritus?"); ?></label><br />
+
+				<input type="checkbox" name="honorary" id="honorary"
+				<?php if (esc_attr($honorary) == "on") { ?>checked<?php } ?> />
+				<label for="honorary"><?php _e("Honorary?"); ?></label>
+			</td>
+		</tr>
+		<?php do_action('largo_more_profile_information', $user); ?>
+	</table>
+<?php }
+add_action( 'show_user_profile', 'more_profile_info' );
+add_action( 'edit_user_profile', 'more_profile_info' );
+
+function save_more_profile_info($user_id) {
+	if (!current_user_can('edit_user', $user_id ))
+		return false;
+
+	update_user_meta($user_id, 'job_title', $_POST['job_title']);
+	update_user_meta($user_id, 'hide', $_POST['hide']);
+	update_user_meta($user_id, 'emeritus', $_POST['emeritus']);
+	update_user_meta($user_id, 'honorary', $_POST['honorary']);
+}
+add_action('personal_options_update', 'save_more_profile_info');
+add_action('edit_user_profile_update', 'save_more_profile_info');
---
+++ inc/widgets/largo-staff.php
@@ -0,0 +1,125 @@
+<?php
+
+class largo_staff_widget extends WP_Widget {
+
+	function __construct() {
+		parent::__construct(
+			'staff_widget', // Base ID
+			'Staff Roster Widget', // Name
+			array( 'description' => 'Display a list of staff members with photos and bios.' ) // Args
+		);
+	}
+
+	public function widget( $args, $instance ) {
+		$title = apply_filters('widget_title', $instance['title']);
+
+		echo $args['before_widget'];
+		if (!empty($title))
+			echo $args['before_title'] . $title . $args['after_title'];
+
+		if (empty($instance['roles'])) {
+			$users = get_users(array(
+				'blog_id' => get_current_blog_id(),
+				'role' => 'author'
+			));
+		} else {
+			$users = array();
+			foreach ($instance['roles'] as $key => $val) {
+				if ($val == 'on') {
+					$result = get_users(array(
+						'blog_id' => get_current_blog_id(),
+						'role' => $key
+					));
+					$users = array_merge($users, $result);
+				}
+			}
+		}
+
+		$markup = '<ul id="staff-roster">';
+		foreach ($users as $user) {
+			if (get_user_meta($user->ID, 'hide', true))
+				continue;
+
+			$avatar = get_avatar($user->ID, '65');
+			$author_url = get_author_posts_url($user->ID);
+			$twitter = get_user_meta($user->ID, 'twitter', true);
+			$emeritus = get_user_meta($user->ID, 'emeritus', true);
+			$honorary = get_user_meta($user->ID, 'honorary', true);
+			$twitter = (!empty($twitter))? $twitter : $author_url;
+			$job_title = get_user_meta($user->ID, 'job_title', true);
+
+			if (!empty($honorary) && !empty($emeritus)) {
+				$job_title = $job_title . ' (Honorary, emeritus)';
+			} else {
+				if (!empty($honorary))
+					$job_title = $job_title . ' (Honorary)';
+
+				if (!empty($emeritus))
+					$job_title = $job_title . ' (Emeritus)';
+			}
+
+			$user_posts_link = '';
+			if (count_user_posts($user->ID) > 0)
+				$user_posts_link = "<a href=\"$author_url\">{$user->first_name}'s posts</a>";
+
+			$markup .= <<<EOD
+<li>
+	<div>
+		<a href="$twitter">
+			$avatar
+			<span class="staff-name">{$user->display_name}</span>
+		</a>
+		<p>$job_title<p>
+		$user_posts_link
+	</div>
+</li>
+EOD;
+		}
+		$markup .= '</ul>';
+		echo $markup;
+		echo $args['after_widget'];
+	}
+
+	public function form( $instance ) {
+		$roles = get_editable_roles();
+
+		if (isset($instance['title']))
+			$title = $instance['title'];
+		else
+			$title = 'Staff Members';
+
+		if (empty($instance['roles']))
+			$instance['roles']['author'] = 'on';
+
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+
+		<p>
+			<label><?php _e( 'Include:' ); ?></label><br/>
+			<?php foreach ($roles as $key => $role) { ?>
+			<label><input <?php checked($instance['roles'][$key], 'on', true); ?>
+					type="checkbox"
+					id="<?php echo $this->get_field_id($key); ?>"
+					name="<?php echo $this->get_field_name($key); ?>"> <?php echo $role['name']; ?>s</label><br />
+			<?php } ?>
+		</p>
+
+		<?php
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = (!empty($new_instance['title']))? strip_tags($new_instance['title']) : '';
+
+		$roles = get_editable_roles();
+		foreach ($roles as $key => $role)
+			$instance['roles'][$key] = (!empty($new_instance[$key]))? $new_instance[$key] : 'off';
+
+		return $instance;
+	}
+
+
+}
