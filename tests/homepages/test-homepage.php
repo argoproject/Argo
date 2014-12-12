5afd8656ca5072ed6ca1670cd68b1fb5f96f1a2c
Ben Keith <ben@benlk.com>
2014-12-12 17:05:34 -0500

    homepage/homepage.php tests should pass now

--- tests/homepages/test-homepage.php
+++ tests/homepages/test-homepage.php
@@ -7,28 +7,32 @@ class HomepageTest extends WP_UnitTestCase {
 	}
 
 	function test_largo_register_default_homepage_layouts() {
-		//if series are enabled	
-		of_set_option('series', true);
-		largo_register_default_homepage_layouts(); // creates $largo_homepage_factory
+		//if series are enabled
+		$GLOBALS['largo_homepage_factory'] = new HomepageLayoutFactory();
 		global $largo_homepage_factory;
+		of_set_option('series_enabled', 1);
+		largo_register_default_homepage_layouts(); // creates $largo_homepage_factory
 		$this->assertCount(5, $largo_homepage_factory->layouts, "Series enabled failed");
 		// cleanup
 		unset($GLOBALS['largo_homepage_factory']);
+		$GLOBALS['largo_homepage_factory'] = new HomepageLayoutFactory();
 
 		//if series are not enabled
-		of_set_option('series', false);
-		largo_register_default_homepage_layouts(); // creates $largo_homepage_factory
 		global $largo_homepage_factory;
+		of_set_option('series_enabled', 0);
+		largo_register_default_homepage_layouts(); // creates $largo_homepage_factory
 		$this->assertCount(4, $largo_homepage_factory->layouts, "Series disabled failed");
 		// cleanup
 		unset($GLOBALS['largo_homepage_factory']);
+		$GLOBALS['largo_homepage_factory'] = new HomepageLayoutFactory();
 	}
 
 	function test_largo_get_home_layouts() {
 		$this->markTestIncomplete('This test has not been implemented yet.');
 		
-		//if series are enabled	
-		of_set_option('series', true);
+		// if series are enabled
+		global $largo_homepage_factory;
+		of_set_option('series_enabled', 1);
 		largo_register_default_homepage_layouts(); // creates $largo_homepage_factory
 		global $largo_homepage_factory;
 		foreach ($largo_homepage_factory->layouts as $key => $value) {
@@ -36,14 +40,19 @@ class HomepageTest extends WP_UnitTestCase {
 		}
 		// cleanup
 		unset($largo_homepage_factory);
+		$GLOBALS['largo_homepage_factory'] = new HomepageLayoutFactory();
 
-		//if series are not enabled
-#		of_set_option('series', false);
-#		largo_register_default_homepage_layouts(); // creates $largo_homepage_factory
-#		global $largo_homepage_factory;
-#		$this->assertEquals(4, count($largo_homepage_factory->layouts));
-#		// cleanup
-#		unset($largo_homepage_factory);
+		// if series are not enabled
+		global $largo_homepage_factory;
+		of_set_option('series_enabled', 0);
+		largo_register_default_homepage_layouts(); // creates $largo_homepage_factory
+		global $largo_homepage_factory;
+		foreach ($largo_homepage_factory->layouts as $key => $value) {
+			assertInternalType('class', $value);
+		}
+		// cleanup
+		unset($largo_homepage_factory);
+		$GLOBALS['largo_homepage_factory'] = new HomepageLayoutFactory();
 	}
 
 	function test_largo_get_home_thumb() {
