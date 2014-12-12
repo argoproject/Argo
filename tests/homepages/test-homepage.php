dc56910fcf9719241db909080cd1580a7371ec3a
Ben Keith <ben@benlk.com>
2014-12-12 15:57:16 -0500

    Better to unset this in the $GLOBALS than locally

--- tests/homepages/test-homepage.php
+++ tests/homepages/test-homepage.php
@@ -11,21 +11,39 @@ class HomepageTest extends WP_UnitTestCase {
 		of_set_option('series', true);
 		largo_register_default_homepage_layouts(); // creates $largo_homepage_factory
 		global $largo_homepage_factory;
-		$this->assertEquals(5, count($largo_homepage_factory->layouts));
+		$this->assertCount(5, $largo_homepage_factory->layouts, "Series enabled failed");
 		// cleanup
-		unset($largo_homepage_factory);
+		unset($GLOBALS['largo_homepage_factory']);
 
 		//if series are not enabled
 		of_set_option('series', false);
 		largo_register_default_homepage_layouts(); // creates $largo_homepage_factory
 		global $largo_homepage_factory;
-		$this->assertEquals(4, count($largo_homepage_factory->layouts));
+		$this->assertCount(4, $largo_homepage_factory->layouts, "Series disabled failed");
 		// cleanup
-		unset($largo_homepage_factory);
+		unset($GLOBALS['largo_homepage_factory']);
 	}
 
-	function test_largo_get_home_layout() {
+	function test_largo_get_home_layouts() {
 		$this->markTestIncomplete('This test has not been implemented yet.');
+		
+		//if series are enabled	
+		of_set_option('series', true);
+		largo_register_default_homepage_layouts(); // creates $largo_homepage_factory
+		global $largo_homepage_factory;
+		foreach ($largo_homepage_factory->layouts as $key => $value) {
+			assertInternalType('class', $value);
+		}
+		// cleanup
+		unset($largo_homepage_factory);
+
+		//if series are not enabled
+#		of_set_option('series', false);
+#		largo_register_default_homepage_layouts(); // creates $largo_homepage_factory
+#		global $largo_homepage_factory;
+#		$this->assertEquals(4, count($largo_homepage_factory->layouts));
+#		// cleanup
+#		unset($largo_homepage_factory);
 	}
 
 	function test_largo_get_home_thumb() {
