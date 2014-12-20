22817c662f4daf69c8377d137d81a3fae771c31e
Ryan Nagle <rmnagle@gmail.com>
2014-12-19 19:41:16 -0600

    add pot and po2mo tasks to gruntfile, regenerate largo pot file, merge es_ES.po with the new pot file and regenerate es_ES.mo

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,40 @@ module.exports = function(grunt) {
             files: 'less/**/*.less',
             tasks: 'less'
           }
+        },
+
+        pot: {
+            options: {
+                text_domain: 'largo',
+                dest: 'lang/',
+                keywords: [ //WordPress localisation functions
+                    '__:1',
+                    '_e:1',
+                    '_x:1,2c',
+                    'esc_html__:1',
+                    'esc_html_e:1',
+                    'esc_html_x:1,2c',
+                    'esc_attr__:1',
+                    'esc_attr_e:1',
+                    'esc_attr_x:1,2c',
+                    '_ex:1,2c',
+                    '_n:1,2',
+                    '_nx:1,2,4c',
+                    '_n_noop:1,2',
+                    '_nx_noop:1,2,3c'
+                ]
+            },
+            files: {
+                src: '**/*.php',
+                expand: true
+            }
+        },
+
+        po2mo: {
+            files: {
+                src: 'lang/*.po',
+                expand: true
+            }
         }
     });
 
