d5abb663fc6c475f79973e4057eefe21769604b4
Will Haynes <haynes24@gmail.com>
2015-01-02 13:18:03 -0600

    Adding test

---
+++ tests/widgets/test-disclaimer-widget.php
@@ -0,0 +1,20 @@
+<?php
+
+// Test functions in inc/avatars/admin.php and inc/avatars/functions.php
+class DisclaimerWidgetTestFunctions extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+
+		include_once dirname(dirname(__DIR__)) . '/inc/widgets.php';
+
+	}
+
+	function test_disabling_disclaimer() {
+
+		
+		
+	}
+
+}
+
