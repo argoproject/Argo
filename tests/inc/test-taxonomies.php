9053a557f6d878a790da3a7958b80f3df43a8b44
Ben Keith <ben@benlk.com>
2015-01-07 17:58:02 -0500

    Tests for largo_is_series_enabled

--- tests/inc/test-taxonomies.php
+++ tests/inc/test-taxonomies.php
@@ -9,10 +9,32 @@ class TaxonomiesTestFunctions extends WP_UnitTestCase {
 		$this->contributor_user_ids = $this->factory->user->create_many(5, array('role' => 'contributor'));
 	}
 	
+	function test_largo_is_series_enabled() {
+		// Series option has not been touched yet (a new install)
+		$result = largo_is_series_enabled();
+		$this->assertFalse($result);
+
+		// Series option is to not enable series
+		of_set_option('series_enabled', false);
+		$result = largo_is_series_enabled();
+		$this->assertFalse($result);
+
+		// Series option is to enable series
+		of_set_option('series_enabled', 1);
+		$result = largo_is_series_enabled();
+		$this->assertTrue($result);
+	}
 	function test_largo_custom_taxonomies(){
 		$this->markTestIncomplete('This test has not been implemented yet.');
 	}
 	function test_largo_post_in_series() {
+		// If series are disabled:
+		of_set_option('series_enabled', false);
+		$result = largo_post_in_series();
+		$this->assertFalse($result);
+
+		// If series are enabled:
+		of_set_option('series_enabled', 1);
 		$this->markTestIncomplete('This test has not been implemented yet.');
 	}
 	function test_largo_custom_taxonomy_terms() {
