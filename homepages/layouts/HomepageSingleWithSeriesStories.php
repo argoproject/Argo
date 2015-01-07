8620caf2843b0e10ec615f848a812a84778a5b5e
Ben Keith <ben@benlk.com>
2015-01-07 18:20:39 -0500

    Preventing disabling Series from blocking HomepageSingleWithSeriesStories top display

--- homepages/layouts/HomepageSingleWithSeriesStories.php
+++ homepages/layouts/HomepageSingleWithSeriesStories.php
@@ -22,25 +22,25 @@ class HomepageSingleWithSeriesStories extends HomepageSingle {
 	function __construct($options=array()) {
 		if ( !largo_is_series_enabled() ){
 			add_action('init', array($this, 'unregister_HomepageSingleWithSeriesStories'), 105);
-		} else {
-			$defaults = array(
-				'name' => __('One big story and list of stories from the same series', 'largo'),
-				'type' => 'series',
-				'description' => __('A single story with full-width image treatment. Series stories appear to the right of the big story\'s headline and excerpt.', 'largo'),
-				'sidebars' => array(
-					__('Home Bottom Left', 'largo'), __('Home Bottom Center', 'largo'), __('Home Bottom Right', 'largo')
-				),
-				'prominenceTerms' => array(
-					array(
-						'name' => __('Homepage Top Story', 'largo'),
-						'description' => __('If you are using the Newspaper or Carousel optional homepage layout, add this label to a post to make it the top story on the homepage', 'largo'),
-						'slug' => 'top-story'
-					)
-				)
-			);
-			$options = array_merge($defaults, $options);
-			parent::__construct($options);
 		}
+
+		$defaults = array(
+			'name' => __('One big story and list of stories from the same series', 'largo'),
+			'type' => 'series',
+			'description' => __('A single story with full-width image treatment. Series stories appear to the right of the big story\'s headline and excerpt.', 'largo'),
+			'sidebars' => array(
+				__('Home Bottom Left', 'largo'), __('Home Bottom Center', 'largo'), __('Home Bottom Right', 'largo')
+			),
+			'prominenceTerms' => array(
+				array(
+					'name' => __('Homepage Top Story', 'largo'),
+					'description' => __('If you are using the Newspaper or Carousel optional homepage layout, add this label to a post to make it the top story on the homepage', 'largo'),
+					'slug' => 'top-story'
+				)
+			)
+		);
+		$options = array_merge($defaults, $options);
+		parent::__construct($options);
 	}
 
 	public function moreStories() {
