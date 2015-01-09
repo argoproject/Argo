db7e320ada40f5b8c431786ee8f83f9093658b5e
Ben Keith <ben@benlk.com>
2015-01-09 11:24:06 -0500

    Placeholder tests file for inc/enqueue.php

---
+++ tests/inc/test-enqueue.php
@@ -0,0 +1,38 @@
+<?php
+
+class EnqueueTestFunctions extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+
+	}
+
+	function test_largo_enqueue_js() {
+	// Modernizr
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	// the jquery plugins
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	// our main JS file
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	// only load sharethis on single pages and load jquery tabs for the related content box if it's active
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	// Load the child theme's style.css if we're actually running a child theme of Largo
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_largo_enqueue_admin_scripts() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_largo_header_js() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_largo_footer_js() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function largo_google_analytics() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+}
