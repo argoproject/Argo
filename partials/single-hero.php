d6b395bc886db4d7d195ba02189ba7d0ce01c4e5
Ryan Nagle <rmnagle@gmail.com>
2015-01-15 22:24:36 -0600

    working on the front end display of featured media

--- partials/single-hero.php
+++ partials/single-hero.php
@@ -1,19 +1,42 @@
 <?php
-$hero_class = largo_hero_class( $post->ID, FALSE );
-$values = get_post_custom( $post->ID );
+$hero_class = largo_hero_class($post->ID, false);
+$values = get_post_custom($post->ID);
 
 // If the box is checked to override the featured image display we'll treat the hero as if it was empty
 // EXCEPT if a featured video is specified
-if ( isset( $values['featured-image-display'][0] ) && !isset( $values['youtube_url'] ) ) $hero_class = 'is-empty';
+if (isset($values['featured-image-display'][0]) && !isset($values['youtube_url']))
+	$hero_class = 'is-empty';
 
-if ( $hero_class != 'is-empty') {
-	echo '<div class="hero span12 ' . $hero_class . '">';
-		if ( $youtube_url = $values['youtube_url'][0] ) {
-			echo '<div class="embed-container">';
-			largo_youtube_iframe_from_url( $youtube_url );
-			echo '</div>';
-		} elseif ( has_post_thumbnail() ) {
-			largo_hero_with_caption( $post->ID );
-		}
-	echo '</div>';
+if (largo_has_featured_media($post->ID)) {
+	$featured_media = largo_get_featured_media($post->ID); ?>
+	<div class="hero span12 <?php echo $hero_class; ?>">
+		<?php if ($featured_media['type'] == 'embed-code') { ?>
+			<div class="embed-container">
+				<?php echo $featured_media['embed']; ?>
+			</div>
+		<?php } else if ($featured_media['type'] == 'video') { ?>
+			<div class="embed-container">
+				<?php echo $featured_media['embed'];
+				if (!empty($featured_media['title'])) { echo '<p class="featured-title">' . $featured_media['title'] . "</p>"; }
+				if (!empty($featured_media['caption'])) { echo '<p class="featured-caption">' . $featured_media['caption'] . "</p>"; }
+				if (!empty($featured_media['credit'])) { echo '<p class="featured-credit">' . $featured_media['credit'] . "</p>"; }
+				?>
+			</div>
+		<?php } else if ($featured_media['type'] == 'image') {
+			largo_hero_with_caption($post->ID);
+		} else if ($featured_media['type'] == 'gallery') {
+			$ids = implode(',', $featured_media['gallery']); ?>
+			<div class="featured-gallery">
+				<?php echo gallery_shortcode(array('ids' => $ids)); ?>
+			</div>
+		<?php } ?>
+		</div>
+	</div>
+<?php } else if ($hero_class != 'is-empty' && $youtube_url = $values['youtube_url'][0]) { ?>
+	<div class="hero span12 <?php echo $hero_class; ?>">
+		<div class="embed-container">
+			<?php largo_youtube_iframe_from_url( $youtube_url ); ?>
+		</div>
+	</div>
+<?php
 }
