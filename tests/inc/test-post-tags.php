9799e34e70f33c835b3681853b3e29ba65de7b22
Ben Keith <ben@benlk.com>
2015-03-25 15:47:57 -0400

    This is a first pass on tests for the largo_post_social_links, since I touched it today.

--- tests/inc/test-post-tags.php
+++ tests/inc/test-post-tags.php
@@ -24,7 +24,65 @@ class PostTagsTestFunctions extends WP_UnitTestCase {
 	}
 
 	function test_largo_post_social_links() {
-		$this->markTestIncomplete("This test has not yet been implemented.");
+		// Create a post, go to it.
+		$id = $this->factory->post->create();
+		$this->go_to('/?p=' . $id);
+
+		// Test the output of this when no options are set
+		$this->assertFalse(of_get_option('article_utilities'));
+
+		ob_start();
+		largo_post_social_links();
+		$ret = ob_get_clean();
+		$this->assertRegExp('/post-social/', $ret, "The .post-social class was not in the output");
+		$this->assertRegExp('/left/', $ret, "The .left class was not in the output");
+		$this->assertRegExp('/right/', $ret, "The .right class was not in the output");
+		unset($ret);
+
+		// Test that this outputs the expected data for each of the button types
+
+		// Twitter
+		of_set_option('article_utilities', array('twitter' => '1', 'facebook' => false, 'print' => false, 'email' => false));
+		of_set_option('twitter_link', 'foo');
+		of_set_option('show_twitter_count', false);
+		ob_start();
+		largo_post_social_links();
+		$ret = ob_get_clean();
+		$this->assertRegExp('/' . preg_quote(of_get_option('twitter_link'), '/') . '/' , $ret, "The Twitter link did not appear in the output");
+		// @TODO: insert a test for the get_the_author_meta test here
+		// This is just a test for make sure that it outputs the data-count when show_twitter_count == 0,; needs another go-round for '1'
+		$this->assertRegExp('/' . __('Tweet', 'largo') . '/', $ret, "The translation of 'Tweet' was not in the Twitter output");
+		unset($ret);
+		of_reset_options();
+
+		// Facebook
+		of_set_option('article_utilities', array('facebook' => '1', 'twitter' => false, 'print' => false, 'email' => false));
+		ob_start();
+		largo_post_social_links();
+		$ret = ob_get_clean();
+		$this->assertRegExp('/' . preg_quote(esc_attr( of_get_option( 'fb_verb' ) ), '/' ) . '/', $ret, "The Facebook Verb was not in the Facebook output");
+		$this->assertRegExp('/' . preg_quote(get_permalink(), '/') . '/', $ret, "The permalink was not in the Facebook output");
+		unset($ret);
+		of_reset_options();
+
+		// Print
+		of_set_option('article_utilities', array('print' => '1', 'twitter' => '1', 'facebook' => false, 'email' => false));
+		ob_start();
+		largo_post_social_links();
+		$ret = ob_get_clean();
+		$this->assertRegExp('/print/', $ret, "The Print output did not include a print class");
+		unset($ret);
+		of_reset_options();
+
+		// Email
+		of_set_option('article_utilities', array('email' => '1', 'twitter' => false, 'facebook' => false, 'print' => false));
+		ob_start();
+		largo_post_social_links();
+		$ret = ob_get_clean();
+		$this->assertRegExp('/email/', $ret, "The Email output did not include an email class");
+		unset($ret);
+		of_reset_options();
+
 	}
 
 	function test_largo_has_avatar() {
