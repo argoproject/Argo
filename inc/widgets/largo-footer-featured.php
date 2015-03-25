68a6164905dedddccf7a8dc01f11ca1caa1057c7
Will Haynes <haynes24@gmail.com>
2015-03-24 21:03:43 -0500

    Reset the darn postdate.

--- inc/widgets/largo-footer-featured.php
+++ inc/widgets/largo-footer-featured.php
@@ -50,10 +50,16 @@ class largo_footer_featured_widget extends WP_Widget {
 
     		<?php endif; // end more featured posts ?>
 
+
+
 		<?php
+		
 		echo $after_widget;
+		
 		// Restore global $post
+		wp_reset_postdata();
 		$post = $preserve;
+
 	}
 
 	function update( $new_instance, $old_instance ) {
