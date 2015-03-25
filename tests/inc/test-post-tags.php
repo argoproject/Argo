710172e96f9e7305228895652f826bad099faa3c
Ben Keith <ben@benlk.com>
2015-03-25 14:45:28 -0400

    Stubbing tests for inc/post-tags.php

---
+++ tests/inc/test-post-tags.php
@@ -0,0 +1,74 @@
+<?php
+
+class PostTagsTestFunctions extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+
+	}
+
+	function test_largo_time() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_author() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_author_link() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_byline() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_post_social_links() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_has_avatar() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_my_queryvars() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_custom_wp_link_pages() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_excerpt() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_trim_sentences() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_content_nav() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_comment() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_post_type_icon() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_hero_class() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_hero_with_caption() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+	function test_largo_post_metadata() {
+		$this->markTestIncomplete("This test has not yet been implemented.");
+	}
+
+}
