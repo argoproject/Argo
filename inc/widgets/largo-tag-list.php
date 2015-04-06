ce72a24cf19d7e18b19a8f775e3bc7c7081323cc
Ben Keith <ben@benlk.com>
2015-04-06 19:18:47 -0400

    One more widget

--- inc/widgets/largo-tag-list.php
+++ inc/widgets/largo-tag-list.php
@@ -14,9 +14,6 @@ class largo_tag_list_widget extends WP_Widget {
 
 	function widget( $args, $instance ) {
 		global $post;
-		// Preserve global $post
-		$preserve = $post;
-
 		extract( $args );
 
 		// only useful on post pages
@@ -37,10 +34,6 @@ class largo_tag_list_widget extends WP_Widget {
     	<?php endif;
 
 		echo $after_widget;
-
-		// Restore global $post
-		wp_reset_postdata();
-		$post = $preserve;
 	}
 
 	function update( $new_instance, $old_instance ) {
