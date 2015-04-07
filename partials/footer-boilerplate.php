c91e99416abe6cfb85b5728d85ff03d728142595
Ben Keith <ben@benlk.com>
2015-04-07 14:04:33 -0400

    Remove the whole div since it'll be blank anyways

--- partials/footer-boilerplate.php
+++ partials/footer-boilerplate.php
@@ -1,16 +1,16 @@
 <div id="boilerplate">
+	<?php if (INN_MEMBER) { ?>
 	<div class="row-fluid clearfix">
 		<div class="span3">
-		<?php if (INN_MEMBER) { ?>
 			<a href="//inn.org/">
 				<img src="<?php echo(get_stylesheet_directory_uri() . "/img/inn_logo_reversetype.png"); ?>" alt="<?php printf(__("%s is a member of the Institute for Nonprofit News", "largo"), get_bloginfo('name')); ?>" />
 			</a>
 			<?php if ( of_get_option('inn_member_since') ) { ?>
 			<p id="inn-since-mobile" class="visible-phone"><?php printf(__("Proud member since %s", "largo"), of_get_option('inn_member_since') ); ?></p>
 			<?php } ?>
-		<?php } ?>
 		</div>
 	</div>
+	<?php } ?>
 
 	<div class="row-fluid clearfix">
 		<div class="span3">
