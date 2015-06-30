0085380fb3f84409c5f0628a413e5e2576614126
Ben Keith <ben@benlk.com>
2015-06-30 11:29:12 -0400

    Re-add defaults for more_link and use_more in largo_excerpt

--- inc/post-tags.php
+++ inc/post-tags.php
@@ -370,7 +370,7 @@ if ( ! function_exists( 'largo_custom_wp_link_pages' ) ) {
  * @since 0.3
  */
 if ( ! function_exists( 'largo_excerpt' ) ) {
-	function largo_excerpt( $the_post=null, $sentence_count = 5, $use_more, $more_link, $echo = true, $strip_tags = true, $strip_shortcodes = true ) {
+	function largo_excerpt( $the_post=null, $sentence_count = 5, $use_more = false, $more_link = '', $echo = true, $strip_tags = true, $strip_shortcodes = true ) {
 		if (!empty($use_more))
 			_deprecated_argument(__FUNCTION__, '0.5.1', 'Parameter $use_more is deprecated.');
 		if (!empty($more_link))
