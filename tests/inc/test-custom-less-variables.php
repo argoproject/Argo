15f2e359f96a65901886ddcff59218bb03ec389b
Ben Keith <ben@benlk.com>
2015-07-31 20:33:30 -0400

    str_replace to achieve protocol agnosticism in compiled LESS css, and a file of blank tests

---
+++ tests/inc/test-custom-less-variables.php
@@ -0,0 +1,140 @@
+<?php
+
+// Test functions in inc/custom-less-variables.php
+class LESSTestFunctions extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+
+	}
+
+	function test_largo_custom_less_cariables_init() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_largo_clv_register_files() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_largo_clv_register_directory_paths() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_largo_clv_register_variables_less_file() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+}
+
+class Largo_Custom_Less_VariablesTest extends WP_UnitTestCaase {
+
+	function test_init() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_put_contents() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_get_contents() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_register_files() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_register_directory_paths() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_register_variables_less_file) {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+
+	function test_get_css() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+
+	function test_compile_less() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_variable_file_path() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_replace_with_custom_variables() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_fix_urls() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_style_loader_src() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_template_redirect() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_success_admin_notices() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_reset_admin_notices() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_admin_menu() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_admin() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_admin_head() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_publish_box() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_get_custom_values() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_get_post() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_reset_all() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_update_custom_values() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_get_editable_variables() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_color_type_field() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_pixels_field() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	function test_dropdown_field() {
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+}
