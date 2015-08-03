eb56acfb5c2fed3bd595417162f6571535dd599e
Ben Keith <ben@benlk.com>
2015-08-03 16:53:34 -0400

    Make the INN logo into a pluggable function

--- partials/footer-boilerplate.php
+++ partials/footer-boilerplate.php
@@ -8,9 +8,7 @@
 
 		<?php if (INN_MEMBER) { ?>
 		<div class="span3">
-			<a href="//inn.org/">
-				<img id="inn-logo" src="<?php echo(get_template_directory_uri() . "/img/inn_logo_reversetype.png"); ?>" alt="<?php printf(__("%s is a member of the Institute for Nonprofit News", "largo"), get_bloginfo('name')); ?>" />
-			</a>
+			<?php inn_logo(); ?>
 			<?php if ( of_get_option('inn_member_since') ) { ?>
 			<p id="mobile-member-since" class="visible-phone"><?php printf(__("Proud member since %s", "largo"), of_get_option('inn_member_since') ); ?></p>
 			<?php } ?>
