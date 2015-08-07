78b9901b18fe88fbcb1807f40b7938159e24aeaf
Ben Keith <ben@benlk.com>
2015-08-07 09:37:54 -0400

    Let's at least use the right assertions

--- tests/homepages/test-homepage-class.php
+++ tests/homepages/test-homepage-class.php
@@ -89,10 +89,10 @@ class HomepageClassTest extends WP_UnitTestCase {
 		$this->layout->enqueueAssets();
 
 		$expected_css_handle = $this->layoutOptions['assets'][0][0];
-		$this->assertTrue(!empty($wp_styles->registered[$expected_css_handle]), "Homepage styles were not enqueued on the homepage");
+		$this->assertNotEmpty($wp_styles->registered[$expected_css_handle], "Homepage styles were not enqueued on the homepage");
 
 		$expected_js_handle = $this->layoutOptions['assets'][1][0];
-		$this->assertTrue(!empty($wp_scripts->registered[$expected_js_handle]), "Homepage scripts were not enqueued on the homepage");
+		$this->assertNotEmpty($wp_scripts->registered[$expected_js_handle], "Homepage scripts were not enqueued on the homepage");
 
 		/*
 		 * Regression test for #791
@@ -101,15 +101,17 @@ class HomepageClassTest extends WP_UnitTestCase {
 		 */
 		$wp_styles->reset();
 		$wp_scripts->reset();
-		$this->go_to('/?p=1'); // a page that is not the homepage
+		// create a post
+		$post_id = $this->factory->post->create();
+		$this->go_to("/?p=$post_id"); // a page that is not the homepage
 
 		$this->layout->enqueueAssets();
 		$expected_css_handle = $this->layoutOptions['assets'][0][0];
-		$this->assertTrue(!empty($wp_styles->registered[$expected_css_handle]), "Homepage styles were not enqueued on the homepage");
+		$this->assertEmpty($wp_styles->registered[$expected_css_handle], "Homepage styles were enqueued on not the homepage");
+		var_log($wp_styles);
 
 		$expected_js_handle = $this->layoutOptions['assets'][1][0];
-		$this->assertTrue(!empty($wp_scripts->registered[$expected_js_handle]), "Homepage scripts were not enqueued on the homepage");
-		var_log($wp_styles);
+		$this->assertEmpty($wp_scripts->registered[$expected_js_handle], "Homepage scripts were enqueued on not the homepage");
 	}
 
 	function testRegisterSidebars() {
