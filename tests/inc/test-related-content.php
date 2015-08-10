9a06c65fba688d319a7cb41b970102f2aff9067e
Ben Keith <ben@benlk.com>
2015-08-10 12:03:39 -0400

    Working tests for __construct

--- tests/inc/test-related-content.php
+++ tests/inc/test-related-content.php
@@ -60,41 +60,47 @@ class RelatedContentTestFunctions extends wp_UnitTestCase{
 }
 
 class LargoRelatedTestFunctions extends WP_UnitTestCase {
+
+	public $cat_id;
+	public $series_id;
+	public $considered;
+
 	function setUp() {
 		parent::setUp();
 
-		$cat_id = $this->factory->category->create();
-		$series_id = $this->factory->term->create(array(
+		$this->cat_id = $this->factory->category->create();
+		$this->series_id = $this->factory->term->create(array(
 			'taxonomy' => 'series'
 		));
-		global $considered;
-		$considered = $this->factory->post->create(array(
-			'post_category' => array($cat_id),
+		$this->considered = $this->factory->post->create(array(
+			'post_category' => array($this->cat_id),
 			'tax_input' => array(
-				'series' => $series_id
+				'series' => $this->series_id
 			)
 		));
 	}
 
 	function test___construct() {
+		$this->go_to("/?p=$this->considered");
 		// check that Largo_Related->number equals 1 if number is not set
 		$lr = new Largo_Related();
 		/*
 		 *
 		 * State of this on Friday evening: I can't create a new Largo_Related in a way that actually triggers it as an object and not as an associative array. 
 		 *
-		 *
-		$this->assertEquals(1, $lr->number);
+		 */
+		$this->assertInstanceOf('Largo_Related', $lr, 'The constructor failed to make $lr an instance of Largo_Related');
+		$this->assertEquals(1, $lr->number, "The number of posts to be returned, when not explicitly set, did not match the programmed number of 1 posts");
 
 		$lr = new Largo_Related(4);
-		$this->assertEquals(4, $lr->number);
+		$this->assertEquals(4, $lr->number, "The number of posts to be returned, when set to 4, was not 4");
 		// check that Largo_Related->post_id is the value of the set post, or the value of the global post
 		global $post;
-		$this->assertEquals($post->id, $lr->post_id);
+		$this->assertEquals($post->ID, $lr->post_id, "The global post's ID does not match the ID of the post considered by the Largo_Related class");
 
-		$lr = new Largo_Related(4, $considered);
-		$this->assertEquals($considered, $lr->post_id);
-		$this->markTestIncomplete('This test has not been implemented yet.');
+		$this->go_to('/'); // So there is no global $post
+		$lr = new Largo_Related(4, $this->considered);
+		$this->assertEquals($this->considered, $lr->post_id, "The post this test is considering does not match the post in the Largo_Related class, when that post ID is explicitly set.");
 	}
 
 	function test_popularity_sort() {
@@ -123,6 +129,11 @@ class LargoRelatedTestFunctions extends WP_UnitTestCase {
 
 	function test_unorganized_series() {
 		of_set_option('series_enabled', 1);
+		$this->factory->posts->create_many(array(
+			'tax_input' => array(
+				'series' => $this->series_id
+			)
+		));
 		$this->markTestIncomplete('This test has not been implemented yet.');
 	}
 
