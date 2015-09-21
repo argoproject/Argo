01198c43ea7e0c23768cbf138ba2b86b8622fa1a
David Ryan <david@inn.org>
2015-09-21 14:16:06 -0700

    resubmitting nav-global
    cleaned up per feedback from ryan

--- partials/nav-global.php
+++ partials/nav-global.php
@@ -1,4 +1,17 @@
 <?php
+/*
+ * Global Navigation Menu
+ *
+ * Shown at the very top of a Largo site on a (default) thin dark bar.
+ * Not shown when sticky navigation is displayed, so typically only the home page.
+ * This menu is appended to the offcanvas nav menu (see partial/nav-sticky.php).
+ *
+ * @link http://largo.readthedocs.org/users/menus.html#available-menu-areas
+ *
+ * @package Largo
+ */
+
+ /* check is homepage and sticky nav is NOT enabled */
 if ( is_front_page() || is_home() || !of_get_option( 'show_sticky_nav' ) ): ?>
 	<div class="global-nav-bg">
 		<div class="global-nav">
@@ -7,6 +20,7 @@ if ( is_front_page() || is_home() || !of_get_option( 'show_sticky_nav' ) ): ?>
 					<a href="#main" title="<?php esc_attr_e( 'Skip to content', 'largo' ); ?>"><?php _e( 'Skip to content', 'largo' ); ?></a>
 				</span>
 				<?php
+					/* Global Navigation Menu Query */
 					$top_args = array(
 						'theme_location' => 'global-nav',
 						'depth'		 => 1,
@@ -15,16 +29,18 @@ if ( is_front_page() || is_home() || !of_get_option( 'show_sticky_nav' ) ): ?>
 					largo_nav_menu($top_args);
 				?>
 				<div class="nav-right">
-					<?php if ( of_get_option( 'show_header_social') ) { ?>
+					<?php
+					/* Check to display Social Media Icons */
+					if ( of_get_option( 'show_header_social') ) { ?>
 						<ul id="header-social" class="social-icons visible-desktop">
 							<?php largo_social_links(); ?>
 						</ul>
 					<?php }
-
+					/* Check to display Donate Button */
 					if ( of_get_option( 'show_donate_button') )
 						largo_donate_button();
 					?>
-
+					<!-- BEGIN Header Search -->
 					<div id="header-search">
 						<form class="form-search" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 							<div class="input-append">
@@ -32,7 +48,7 @@ if ( is_front_page() || is_home() || !of_get_option( 'show_sticky_nav' ) ): ?>
 							</div>
 						</form>
 					</div>
-
+				<!-- END Header Search -->
 				</div>
 			</nav>
 		</div> <!-- /.global-nav -->
