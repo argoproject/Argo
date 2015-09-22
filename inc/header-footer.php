d3a828065f9ef8c5d466597445a4e6fce49d44c6
David Ryan <david@inn.org>
2015-09-22 08:38:16 -0700

    logo change
    i can’t fathom how but this url was changed

--- inc/header-footer.php
+++ inc/header-footer.php
@@ -76,7 +76,7 @@ if ( ! function_exists( 'INN_logo' ) ) {
 	function inn_logo() {
 		?>
 			<a href="//inn.org/" id="inn-logo-container">
-				<img id="inn-logo" src="<?php echo(get_template_directory_uri() . "/img/inn_logo_reversetype.png"); ?>" alt="<?php printf(__("%s is a member of the Institute for Nonprofit News", "largo"), get_bloginfo('name')); ?>" />
+				<img id="inn-logo" src="<?php echo(get_template_directory_uri() . "/img/inn_logo_gray.png"); ?>" alt="<?php printf(__("%s is a member of the Institute for Nonprofit News", "largo"), get_bloginfo('name')); ?>" />
 			</a>
 		<?php
 	}
