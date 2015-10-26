57119ff1f928d0c12a7e1ccbac2a8e5968f5993d
Ryan Nagle <rmnagle@gmail.com>
2015-10-26 13:51:43 -0500

    use the featured posts widget in the fallback sidebar instead of recent posts to replace the deprecated 'recommended' widget -- this matches what happens during the update/replacement of deprecated widgets

--- partials/sidebar-fallback.php
+++ partials/sidebar-fallback.php
@@ -24,11 +24,12 @@ if (of_get_option('donate_link')) {
 	);
 }
 
-the_widget( 'largo_recent_posts_widget', array(
-		'title' => __('Recent Posts', 'largo'),
+the_widget( 'largo_featured_widget', array(
+		'term' => 'sidebar-featured',
+		'title' => __('We Recommend', 'largo'),
+		'widget_class' => 'default',
 		'num_posts' => 5,
 		'num_sentences' => 2,
-		'widget_class' => 'default'
 	)
 );
 
