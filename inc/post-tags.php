3c507a35dc88c49bcd8ea40d82c050cd9d99a251
Ben Keith <ben@benlk.com>
2015-11-12 16:03:44 -0500

    Use real numbers

--- inc/post-tags.php
+++ inc/post-tags.php
@@ -727,8 +727,8 @@ if ( ! function_exists('largo_floating_social_button_width_json') ) {
 	function largo_floating_social_button_width_json() {
 		if ( is_single() && of_get_option('single_floating_social_icons') == '1' && of_get_option('single_template') == 'normal' ) {
 			$config = array(
-				'min' => '769px',
-				'max' => '9999px',
+				'min' => '769',
+				'max' => '9999',
 			);
 			$config = apply_filters( 'largo_floating_social_button_width_json', $config );
 			?>
