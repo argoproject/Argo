834600e5ac048557ea24f8d894f6a6423dab7173
Ryan Nagle <rmnagle@gmail.com>
2015-11-23 16:46:19 -0600

    running 'grunt build-release' ahead of v0.5.3 release

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ copyright = u'2014, Institute for Nonprofit News'
 # built documents.
 #
 # The short X.Y version.
-version = '0.5.2'
+version = '0.5.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
