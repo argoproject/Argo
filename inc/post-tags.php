b4f0b14bb17f19120eb2c0d186943d27cbeb66e5
Ryan Nagle <rmnagle@gmail.com>
2015-12-10 13:38:48 -0600

    make sure the taxonomy link and feed link are not WP_Error objects before adding them to the list of 'more' social links

--- inc/post-tags.php
+++ inc/post-tags.php
@@ -207,10 +207,14 @@ if ( ! function_exists( 'largo_post_social_links' ) ) {
 
 		if ( ! empty( $top_term_id ) ) {
 			$top_term_link = get_term_link( $top_term_id, $top_term_taxonomy );
-			$more_social_links[] = '<li><a href="' . $top_term_link . '"><i class="icon-link"></i> More on this topic</a></li>';
+			if ( ! is_wp_error( $top_term_link ) ) {
+				$more_social_links[] = '<li><a href="' . $top_term_link . '"><i class="icon-link"></i> More on this topic</a></li>';
+			}
 
 			$top_term_feed_link = get_term_feed_link( $top_term_id, $top_term_taxonomy );
-			$more_social_links[] = '<li><a href="' . $top_term_feed_link . '"><i class="icon-rss"></i> Subscribe to this topic</a></li>';
+			if ( ! is_wp_error( $top_term_feed_link ) ) {
+				$more_social_links[] = '<li><a href="' . $top_term_feed_link . '"><i class="icon-rss"></i> Subscribe to this topic</a></li>';
+			}
 		}
 
 		// Try to get the author's Twitter link
