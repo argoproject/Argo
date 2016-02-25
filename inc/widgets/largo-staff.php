787d4b0bec0021f970c5578ee9a9d1060f8aefd1
Adam Schweigert <aschweigert@gmail.com>
2016-02-25 13:04:12 -0500

    minor format change
    + make sure we have a job title before we try to display it

--- inc/widgets/largo-staff.php
+++ inc/widgets/largo-staff.php
@@ -43,8 +43,10 @@ class largo_staff_widget extends WP_Widget {
 			if ($hide == 'on')
 				continue;
 
-			$job_title = get_user_meta($user->ID, 'job_title', true);
-			$avatar_alt = esc_attr( $user->first_name . ' ' . $user->last_name . ', ' . $job_title );
+			$avatar_alt = esc_attr( $user->first_name . ' ' . $user->last_name );
+			if ( $job_title = get_user_meta( $user->ID, 'job_title', true ) ) {
+				$avatar_alt .= ' (' . $job_title . ')';
+			}
 			$avatar = get_avatar($user->ID, '65', null, $avatar_alt);
 			$author_url = get_author_posts_url($user->ID);
 
