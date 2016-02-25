e66d938e97f1198af4488b2669b235cd41012632
Ben Keith <ben@benlk.com>
2016-02-25 12:43:26 -0500

    Add name and job title to the alt attribute of images in the Largo Staff Widget

--- inc/widgets/largo-staff.php
+++ inc/widgets/largo-staff.php
@@ -43,9 +43,10 @@ class largo_staff_widget extends WP_Widget {
 			if ($hide == 'on')
 				continue;
 
-			$avatar = get_avatar($user->ID, '65');
-			$author_url = get_author_posts_url($user->ID);
 			$job_title = get_user_meta($user->ID, 'job_title', true);
+			$avatar_alt = esc_attr( $user->first_name . ' ' . $user->last_name . ', ' . $job_title );
+			$avatar = get_avatar($user->ID, '65', null, $avatar_alt);
+			$author_url = get_author_posts_url($user->ID);
 
 			$user_posts_link = '';
 			if (count_user_posts($user->ID) > 0)
