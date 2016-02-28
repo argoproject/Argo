89524db0b02a2ba5d6e111db814d0dddc6ab5811
aschweigert <aschweigert@gmail.com>
2016-02-28 13:32:41 -0500

    update doc block

--- inc/plugin-init.php
+++ inc/plugin-init.php
@@ -1,13 +1,12 @@
 <?php
 /**
  * @package    TGM-Plugin-Activation
- * @subpackage Example
  * @version    2.5.2 for parent theme Largo for publication on WordPress.org
  * @author     Thomas Griffin, Gary Jones, Juliette Reinders Folmer
  * @copyright  Copyright (c) 2011, Thomas Griffin
  * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
  * @link       https://github.com/TGMPA/TGM-Plugin-Activation
- * @see http://tgmpluginactivation.com/configuration/ for detailed documentation.
+ * @see 		http://tgmpluginactivation.com/configuration/ for detailed documentation.
  */
 
 /**
