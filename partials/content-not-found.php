f90cbf072a1ef185b625d01a5a7a65de9315d286
Ben Keith <ben@benlk.com>
2016-03-10 11:40:08 -0700

    Change apologies in partials/no-content if it's a 404, to include the URL, and add search to the search form if the search has been saerched.

--- partials/content-not-found.php
+++ partials/content-not-found.php
@@ -1,10 +1,21 @@
+<?php
+$apologies = __('Apologies, but no results were found. Perhaps searching will help.', 'largo');
+
+if ( is_404() ) {
+	$apologies = sprintf(
+		__("Apologies, but <code>%s</code> was not found. Perhaps wearching will help.", 'largo'),
+		wp_kses($_SERVER['REQUEST_URI'], array()) // The url
+	);
+}
+
+?>
 <article id="post-0" class="post no-results not-found">
 	<header class="entry-header">
 		<h1 class="entry-title"><?php _e('Nothing Found', 'largo'); ?></h1>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<p><?php _e('Apologies, but no results were found. Perhaps searching will help.', 'largo'); ?></p>
+		<p><?php echo $apologies; ?></p>
 		<?php get_search_form(); ?>
 	</div><!-- .entry-content -->
 </article><!-- #post-0 -->
