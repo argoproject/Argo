f7b811f197545ffde69cffd892a0e0bb71971365
Ben Keith <ben@benlk.com>
2016-03-16 10:43:16 -0400

    Display Recemt Posts widget in 404 page with default args

--- 404.php
+++ 404.php
@@ -19,6 +19,25 @@ get_header(); ?>
 			'strong' => array(),
 		));
 	?></p>
+
+	<?php
+		/*
+		 * Display the Recent Posts widget
+		 *
+		 * @since 0.5.5
+		 * @link http://jira.inn.org/browse/WE-103
+		 * @link https://codex.wordpress.org/Function_Reference/the_widget
+		 */
+		if ( class_exists( 'largo_recent_posts_widget' ) ) {
+			the_widget(
+				'largo_recent_posts_widget',
+				array (
+				),
+				array(
+				)
+			);
+		}
+	?>
 </div><!--#content -->
 
 <?php get_sidebar(); ?>
