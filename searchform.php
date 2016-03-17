32980f9bc1a92b765b64dbfdea62e15735f191fd
Ben Keith <ben@benlk.com>
2016-03-17 16:09:03 -0400

    Fix a problem with the merge in searchform.php

--- searchform.php
+++ searchform.php
@@ -5,7 +5,6 @@
 ?>
 <form class="form-search" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="input-append">
-		<input type="text" placeholder="<?php _e('Search', 'largo'); ?>" class="searchbox search-query" value="<?php the_search_query(); ?>" name="s" />
-		<input type="submit" value="<?php _e('Go', 'largo'); ?>" name="search submit" class="search-submit btn">
+		<input type="text" placeholder="<?php _e('Search', 'largo'); ?>" class="searchbox search-query" value="<?php the_search_query(); ?>" name="s" /><button type="submit" name="search submit" class="search-submit btn"><?php _e('Go', 'largo'); ?></button>
 	</div>
 </form>
