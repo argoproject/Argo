7d7ea251e90f51a5137c276dc80d7a84d34fe4c8
Kirsten Lambertsen <kirsten@kirstenlambertsen.com>
2016-07-11 00:52:07 -0400

    updates deprecated get_currentuserinfo to wp_get_current_user for WP v. 4.5 and higher as listed in issue #844

--- inc/users.php
+++ inc/users.php
@@ -140,8 +140,13 @@ function largo_edit_permission_check() {
     global $current_user, $profileuser;
 
     $screen = get_current_screen();
-
-    wp_get_current_user();
+    
+    global $wp_version; 
+    if ( !substr( $wp_version, 0, 3 ) === "4.5" ) {
+    	get_currentuserinfo();
+    } else {
+    	wp_get_current_user();
+    }
 
     if( ! is_super_admin( $current_user->ID ) && in_array( $screen->base, array( 'user-edit', 'user-edit-network' ) ) ) { // editing a user profile
         if ( is_super_admin( $profileuser->ID ) ) { // trying to edit a superadmin while less than a superadmin
