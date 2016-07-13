dcb5c3021dd260bbca0a9585c484d483271e9dd1
Ben Keith <ben@benlk.com>
2016-07-13 13:56:01 -0400

    Add Largo_CoAuthors_Byline class extending Largo_Byline, with support for Co-Authors Plus guest users

--- inc/byline_class.php
+++ inc/byline_class.php
@@ -28,6 +28,7 @@ class Largo_Byline {
 		$this->author_link();
 		$this->job_title();
 		$this->twitter();
+
 		$this->published_date();
 		$this->edited_date();
 		$this->edit_link();
@@ -98,6 +99,7 @@ class Largo_Byline {
 		if (
 			current_user_can( 'edit_post', $this->post_id )
 			&& ! $this->exclude_date
+			&& is_single()
 		) {
 			printf(
 				' <span class="last-modified">%1$s %2$s %3$s %4$s</span',
@@ -140,5 +142,92 @@ class Largo_Custom_Byline extends Largo_Byline {
 	}
 }
 
+/**
+ * Bylines for Co-Authors Plus guest authors
+ */
 class Largo_CoAuthors_Byline extends Largo_Byline {
+
+	function generate_byline() {
+		// get the coauthors for this post
+		$coauthors = get_coauthors( $this->post_id );
+		$out = array();
+		// loop over them
+		foreach( $coauthors as $author ) {
+			$this->author_id = $author->ID;
+			$this->author = $author;
+
+			ob_start();
+
+			$this->avatar();
+			$this->author_link();
+			$this->job_title();
+			$this->organization();
+			$this->twitter();
+
+			$byline_temp = ob_get_clean();
+
+			// array of byline html strings
+			$out[] = $byline_temp;
+		}
+
+		// If there are multiple coauthors, join them with commas and 'and'
+		if ( count($out) > 1 ) {
+			end($out);
+			$key = key($out);
+			reset($out);
+			$authors = implode( ', ', array_slice( $out, 0, -1 ) );
+			$authors .= ' <span class="and">' . __( 'and', 'largo' ) . '</span> ' . $out[$key];
+		} else {
+			$authors = $out[0];
+		}
+		echo $authors;
+
+		$this->published_date();
+		$this->edited_date();
+		$this->edit_link();
+
+		$this->output = ob_get_clean();
+	}
+
+	// The author ID for a guest author is the ID of the post that ontains it.
+	function avatar() {
+		$output = get_the_post_thumbnail(
+			$this->author_id,
+			array(32, 32), // size of thing
+			array('avatar')
+		);
+		$output .= ' '; // to reduce run-together bylines
+		echo $output;
+	}
+
+	function author_link() {
+		$author_name = ( !empty($this->author->display_name) ) ? $this->author->display_name : $this->author->user_nicename ;
+
+		$output = '<a class="url fn n" href="' . get_author_posts_url( $this->author->ID, $this->author->user_nicename ) . '" title="' . esc_attr( sprintf( __( 'Read All Posts By %s', 'largo' ), $author_name ) ) . '" rel="author">' . esc_html( $author_name ) . '</a>';
+		echo $output;
+	}
+
+	function job_title() {
+		$show_job_titles = of_get_option('show_job_titles');
+		// only do this if we're showing job titles and there is one to be shown
+		if ( true && $job = $this->author->job_title ) {
+			$output .= '<span class="job-title"><span class="comma">,</span> ' . $job . '</span>';
+		}
+		$output .= '';
+		echo $output;
+	}
+
+	function organization() {
+		if ( $org = $this->author->organization ) {
+			$byline_text = ' (' . $org . ')';
+			echo $byline_text;
+		}
+	}
+
+	function twitter() {
+		if ( isset($this->author->twitter) && is_single() ) {
+			$output .= ' <span class="twitter"><a href="https://twitter.com/' . largo_twitter_url_to_username( $this->author->twitter ) . '"><i class="icon-twitter"></i></a></span>';
+		}
+		echo $output;
+	}
 }
