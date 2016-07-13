c62bb015390813529161fcbf77e109c675313ac7
Ben Keith <ben@benlk.com>
2016-07-13 00:31:18 -0400

    Revert "Move largo_hero_with_caption into inc/deprecated.php, because it is"
    This reverts commit a30deda5a8cddc08c3b6ba732cba92cc159728a2.

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -15,16 +15,3 @@ function largo_cached_nav_menu( $args = array(), $prime_cache = false ) {
 	_deprecated_function(__FUNCTION__, '0.4', 'largo_nav_menu');
 	return largo_nav_menu($args);
 }
-
-/**
- * Returns the featured image for a post; deprecated in 0.5.1
- * to be used as the hero image with caption and credit (if available)
- *
- * @since 0.4
- * @deprecated 0.5.1
- */
-if ( ! function_exists( 'largo_hero_with_caption' ) ) {
-	function largo_hero_with_caption( $post_id ) {
-		largo_featured_image_hero($post_id);
-	}
-}
