65abc0e40940b49dbc683825fab4a72c7ce57fd7
Ben Keith <ben@benlk.com>
2016-07-14 18:12:01 -0400

    Fix bug where largo_get_avatar_src was passing email addresses to largo_get_user_avatar_id

--- inc/avatars/functions.php
+++ inc/avatars/functions.php
@@ -8,20 +8,35 @@ define('LARGO_AVATAR_META_NAME', 'largo_avatar');
 define('LARGO_AVATAR_ACTION_NAME', 'largo_avatar_upload');
 define('LARGO_AVATAR_INPUT_NAME', 'largo_user_avatar');
 
-if (is_admin())
-	include dirname(__FILE__) . '/admin.php';
+if ( is_admin() )
+	include dirname( __FILE__ ) . '/admin.php';
 
-function largo_get_avatar_filter($avatar, $id_or_email, $size, $default, $alt ) {
-	$image_src = largo_get_avatar_src($id_or_email, $size);
-	if (!empty($image_src))
+function largo_get_avatar_filter( $avatar, $id_or_email, $size, $default, $alt ) {
+	$image_src = largo_get_avatar_src( $id_or_email, $size );
+	if ( ! empty( $image_src ) ) {
 		return '<img src="' . $image_src[0] . '" alt="'. $alt . '" width="' . $size . '" height="' . $size . '" class="avatar avatar-' . $size . ' photo" />';
-	else
+	} else {
 		return $avatar;
+	}
 }
 add_action('get_avatar', 'largo_get_avatar_filter', 1, 5);
 
+/**
+ * Get the avatar image HTML for the given user id/email and size
+ *
+ * @param int|string $id_or_email a wordpress user ID or user email address;
+ * @param int $string The size of the avatar
+ */
 function largo_get_avatar_src($id_or_email, $size) {
-	$avatar_id = largo_get_user_avatar_id($id_or_email);
+	// get the user ID;
+	if ( ! is_numeric( $id_or_email ) ) {
+		$user = get_user_by( 'email', $id_or_email );
+		$id = $user->ID;
+	} else {
+		$id = (int) $id_or_email;
+	}
+
+	$avatar_id = largo_get_user_avatar_id( $id );
 
 	if (empty($avatar_id))
 		return false;
