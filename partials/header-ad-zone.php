ecdc31c053e54ec05cbd3b98fe56c6e1b295075b
aschweigert <aschweigert@gmail.com>
2016-08-25 15:53:59 -0400

    avoid outputting an empty div here even when the ad zone is disabled

--- partials/header-ad-zone.php
+++ partials/header-ad-zone.php
@@ -1,8 +1,8 @@
 <?php
-if (is_active_sidebar('header-ads')) { ?>
+if ( is_active_sidebar( 'header-ads' ) ) { ?>
 <div class="header-ad-zone max-wide">
 	<div id="header-ad-zone-container">
-		<?php dynamic_sidebar('header-ads'); ?>
+		<?php dynamic_sidebar( 'header-ads' ); ?>
 	</div>
 </div>
 <?php } ?>
