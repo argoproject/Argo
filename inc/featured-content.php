426a5208058c5d9bea09cc9111c3693e6df5772a
aschweigert <aschweigert@gmail.com>
2016-08-29 21:45:03 -0400

    rebasing this changeset to merge with develop

--- inc/featured-content.php
+++ inc/featured-content.php
@@ -6,7 +6,7 @@
  *
  * @param array $args query args
  * @return array of featured post objects
- * @since 1.0
+ * @since 0.3
  */
 function largo_get_featured_posts( $args = array() ) {
     $defaults = array(
@@ -35,10 +35,10 @@ function largo_get_featured_posts( $args = array() ) {
  *
  * @uses global $post
  * @return term object|false
- * @since 1.0
+ * @since 0.3
  */
-function largo_get_the_main_feature($featured = null) {
-	if ($featured == null) {
+function largo_get_the_main_feature( $featured = null ) {
+	if ( $featured == null ) {
 		global $post;
 		$featured = $post;
 	}
@@ -116,3 +116,69 @@ function largo_have_homepage_featured_posts() {
 	return $featured_query->have_posts();
 
 }
+
+/**
+ * Get posts marked as "Featured in category" for a given category name.
+ *
+ * @param string $category_name the category to retrieve featured posts for.
+ * @param integer $number total number of posts to return, backfilling with regular posts as necessary.
+ * @since 0.5
+ */
+function largo_get_featured_posts_in_category( $category_name, $number = 5 ) {
+	$args = array(
+		'category_name' => $category_name,
+		'numberposts' => $number,
+		'post_status' => 'publish',
+	);
+
+	$tax_query = array(
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'prominence',
+				'field' => 'slug',
+				'terms' => 'category-featured',
+			)
+		)
+	);
+
+	// Get the featured posts
+	$featured_posts = get_posts( array_merge( $args, $tax_query ) );
+
+	// Backfill with regular posts if necessary
+	if ( count( $featured_posts ) < (int) $number ) {
+		$needed = (int) $number - count( $featured_posts );
+		$regular_posts = get_posts( array_merge( $args, array(
+			'numberposts' => $needed,
+			'post__not_in' => array_map( function( $x ) { return $x->ID; }, $featured_posts )
+		)));
+		$featured_posts = array_merge( $featured_posts, $regular_posts );
+	}
+
+	return $featured_posts;
+}
+
+/**
+ * Helper for getting posts in a category archive, excluding featured posts.
+ * 
+ * @param WP_Query $query
+ * @uses largo_get_featured_posts_in_category
+ * @since 0.4
+ */
+function largo_category_archive_posts( $query ) {
+	//don't muck with admin, non-categories, etc
+	if ( ! $query->is_category() || ! $query->is_main_query() || is_admin() ) return;
+
+	// If this has been disabled by an option, do nothing
+	if ( of_get_option( 'hide_category_featured' ) == true ) return;
+
+	// get the featured posts
+	$featured_posts = largo_get_featured_posts_in_category( $query->get( 'category_name' ) );
+
+	// get the IDs from the featured posts
+	$featured_post_ids = array();
+	foreach ( $featured_posts as $fpost )
+		$featured_post_ids[] = $fpost->ID;
+
+	$query->set( 'post__not_in', $featured_post_ids );
+}
+add_action( 'pre_get_posts', 'largo_category_archive_posts', 15 );
