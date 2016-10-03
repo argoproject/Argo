b3798b5db5919924d6937f85643c5097087a770a
Ben Keith <ben@benlk.com>
2016-10-03 12:36:54 -0400

    remove youtube embed from post rivers

--- partials/content.php
+++ partials/content.php
@@ -40,16 +40,12 @@ if ( $featured ) {
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
 
 	<?php
-		// Special treatment for posts that are in the Homepage Featured prominence taxonomy term and have thumbnails or videos.
+		// Special treatment for posts that are in the Homepage Featured prominence taxonomy term and have thumbnails
 		if ( $featured && ( has_post_thumbnail() || $values['youtube_url'] ) ) { ?>
 			<header>
 				<div class="hero span12 <?php echo $hero_class; ?>">
 				<?php
-					if ( $youtube_url = $values['youtube_url'][0] ) {
-						echo '<div class="embed-container">';
-						largo_youtube_iframe_from_url( $youtube_url );
-						echo '</div>';
-					} elseif( has_post_thumbnail() ){
+					if( has_post_thumbnail() ){
 						echo( '<a href="' . get_permalink() . '" title="' . the_title_attribute( array( 'before' => __( 'Permalink to', 'largo' ) . ' ', 'echo' => false )) . '" rel="bookmark">' );
 						the_post_thumbnail( 'full' );
 						echo( '</a>' );
