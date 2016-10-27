d53c65375641b5b44dafa90e7c988a02a346bf85
Ben Keith <ben@benlk.com>
2016-10-27 18:39:53 -0400

    Use correct variable to find the author's ID when it's a coauthor that doesn't have a Largo/Wordpress thumbnail but does have a CoAuthors Plus on. Also fix empty Twitter links

--- inc/byline_class.php
+++ inc/byline_class.php
@@ -89,6 +89,7 @@ class Largo_Byline {
 
 	/**
 	 * On single posts, output the avatar for the author object
+	 * This supports both Largo_Byline and Largo_CoAuthors_Byline
 	 */
 	function avatar() {
 		
@@ -105,11 +106,11 @@ class Largo_Byline {
 					get_the_author_meta( 'display_name', $this->author_id )
 				);
 			} elseif ( $this->author->type == 'guest-author' && get_the_post_thumbnail( $this->author->ID ) ) {
-				$output = get_the_post_thumbnail( $this->author_id, array( 32,32 ) );
+				$output = get_the_post_thumbnail( $this->author->ID, array( 32,32 ) );
 				$output = str_replace( 'attachment-32x32 wp-post-image', 'avatar avatar-32 photo', $output );
 			}
-			$output .= ' '; // to reduce run-together bylines	
-		}	
+			$output .= ' '; // to reduce run-together bylines
+		}
 		echo $output;
 	}
 
@@ -209,6 +210,12 @@ class Largo_CoAuthors_Byline extends Largo_Byline {
 	protected $author;
 
 	/**
+	 * Temporary variable used for the author ID;
+	 * @see $this->generate_byline();
+	 */
+	private $author_id;
+
+	/**
 	 * Differs from Largo_Byline in following ways:
 	 *
 	 * - gets list of coauthors, runs avatar, author_link, job_title, organization, twitter for each of those
@@ -298,7 +305,7 @@ class Largo_CoAuthors_Byline extends Largo_Byline {
 	 */
 	function twitter() {
 		$output = '';
-		if ( isset( $this->author->twitter ) && is_single() ) {
+		if ( ! empty( $this->author->twitter ) && is_single() ) {
 			$output .= ' <span class="twitter"><a href="https://twitter.com/' . largo_twitter_url_to_username( $this->author->twitter ) . '"><i class="icon-twitter"></i></a></span>';
 		}
 		echo $output;
