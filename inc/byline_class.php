a168d84b22281172c006c251aba2b4340789a807
Ben Keith <ben@benlk.com>
2016-11-10 14:24:06 -0500

    Remove var_logs

--- inc/byline_class.php
+++ inc/byline_class.php
@@ -102,9 +102,6 @@ class Largo_Byline {
 			$output = '';
 		} else {
 			$author_email = get_the_author_meta( 'email', $this->author_id );
-			var_log( $this->author->ID );
-			var_log( $this->author_id );
-			var_log( $author_email );
 			if ( $this->author->type == 'guest-author' && get_the_post_thumbnail( $this->author->ID ) ) {
 				$output = get_the_post_thumbnail( $this->author->ID, array( 32,32 ) );
 				$output = str_replace( 'attachment-32x32 wp-post-image', 'avatar avatar-32 photo', $output );
